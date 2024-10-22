7c159140b2428ae3decf53b598b72d946c6752f4
Aarna chauhan <chauhanaarna6@gmail.com>
2024-10-22 22:55:01 +0530

    Create two sum v1 better.cpp

---
+++ Array/two sum better.cpp
@@ -0,0 +1,15 @@
+#include <map>
+string read(int n, vector<int> book, int target)
+{
+    // Write your code here.
+    map<int,int> mpp;
+    for(int i=0;i<n;i++){
+        int a=book[i];
+        int more=target - a;
+        if(mpp.find(more)!=mpp.end()){
+            return "YES";
+        }
+        mpp[a]=i;
+    }
+    return "NO";
+}
