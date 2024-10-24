98b11bd6f22d63ea28fc14ba7f7dbaee9fcc3b15
Aarna chauhan <chauhanaarna6@gmail.com>
2024-10-24 23:14:12 +0530

    Create maximum subarray sum.cpp

---
+++ Array/maximum subarray sum.cpp
@@ -0,0 +1,20 @@
+Kadane's Algorithm : Maximum Subarray Sum in an Array
+long long maxSubarraySum(vector<int> arr, int n)
+{
+    long long maxsum=LONG_MIN;
+    long long sum=0;
+    for (int i = 0; i < n; i++) {
+      sum += arr[i];
+    
+    if(sum>maxsum){
+        maxsum=sum;
+    }
+    if (sum < 0) {
+        sum = 0;
+    }
+    }
+    if(maxsum<0){
+        maxsum=0;
+    }
+    return maxsum;
+}
