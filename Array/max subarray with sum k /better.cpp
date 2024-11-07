174892d56772e87c36ff85cd03d0e9a6dd18be3e
Aarna chauhan <chauhanaarna6@gmail.com>
2024-11-07 21:59:38 +0400

    Update better.cpp

--- Array/max subarray with sum k /better.cpp
+++ Array/max subarray with sum k /better.cpp
@@ -1,1 +1,20 @@
+//positive elements only
+//tc=o(n^2)
 
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+    int n = nums.size(); // size of the array.
+
+    int len = 0;
+    for (int i = 0; i < n; i++) { // starting index
+     int s = 0;
+        for (int j = i; j < n; j++) { // ending index
+           s+=nums[j];
+            if (s == k)
+                len = max(len, j - i + 1);
+        }
+     }
+    return len;
+    }
+};
