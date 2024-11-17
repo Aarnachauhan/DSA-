512f1ac63a68ad8b5d142f217ae44a7442aee670
Aarna chauhan <chauhanaarna6@gmail.com>
2024-11-17 23:25:28 +0530

    Create brute.cpp

---
+++ Array/set matrix zeroes/brute.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+void rows(vector<vector<int>> &matrix,int m ,int n,int i){
+    for(int j=0;j<n;j++){
+        if(matrix[i][j]!=0){
+            matrix[i][j]=-1;
+        }
+    }
+}
+void cols(vector<vector<int>> &matrix,int m,int n,int j){
+    for(int i=0;i<m;i++){
+      if(matrix[i][j]!=0){
+        matrix[i][j]=-1;
+      }
+    }
+}
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    rows(matrix,m,n,i);
+                    cols(matrix,m,n,j);
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+
+    }
+};
