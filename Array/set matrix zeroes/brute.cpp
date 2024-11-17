a722acd89ff2fb5823dba8e6b66961d2c734cd88
Aarna chauhan <chauhanaarna6@gmail.com>
2024-11-17 23:29:06 +0530

    Update brute.cpp

--- Array/set matrix zeroes/brute.cpp
+++ Array/set matrix zeroes/brute.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-void rows(vector<vector<int>> &matrix,int m ,int n,int i){
-    for(int j=0;j<n;j++){
+void rows(vector<vector<int>> &matrix,int m ,int n,int i){ 
+    for(int j=0;j<n;j++){ //n
         if(matrix[i][j]!=0){
             matrix[i][j]=-1;
         }
     }
 }
 void cols(vector<vector<int>> &matrix,int m,int n,int j){
-    for(int i=0;i<m;i++){
+    for(int i=0;i<m;i++){ //m
       if(matrix[i][j]!=0){
         matrix[i][j]=-1;
       }
@@ -18,15 +18,15 @@ void cols(vector<vector<int>> &matrix,int m,int n,int j){
         int m=matrix.size();
         int n=matrix[0].size();
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<n;j++){ //n * m
                 if(matrix[i][j]==0){
                     rows(matrix,m,n,i);
                     cols(matrix,m,n,j);
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<m;i++){  
+            for(int j=0;j<n;j++){  //n*m
                 if(matrix[i][j]==-1){
                     matrix[i][j]=0;
                 }
@@ -35,3 +35,4 @@ void cols(vector<vector<int>> &matrix,int m,int n,int j){
 
     }
 };
+//tc - (n*m) * (n+m) + (n*m)= ( )^3
