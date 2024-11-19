1f01b6c25a744c9cae5a15a77f894d1c9917ee03
Aarna chauhan <chauhanaarna6@gmail.com>
2024-11-19 22:37:14 +0530

    Create next permutation.cpp

---
+++ Array/next permutation.cpp
@@ -0,0 +1,30 @@
+//tc- o(3n)
+//sc-o(n) if we are considering modifying array as sc otherwise o(1)
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int ind=-1;
+      //here we are finding break point
+        for(int i=n-2;i>=0;i--){ 0(n)
+            if(arr[i]<arr[i+1]){
+                ind=i;
+                break;
+            }
+        }
+      //if the permutation- [1,2,3,4,5] -> [5,4,3,2,1] and vice versa 
+        if(ind==-1){
+            reverse(arr.begin(),arr.end());
+             return;
+        }
+      //finding smallest element that is greater than ind
+        for(int i=n-1;i>ind;i--){ //o(n)
+            if(arr[i]>arr[ind]){
+                swap(arr[i],arr[ind]);
+                break;
+            }
+        }
+        reverse(arr.begin()+ind+1,arr.end()); o(n)
+    }
+};
