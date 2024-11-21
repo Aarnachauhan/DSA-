feeea7b3a1caf04e5c3ba8b584803a5889cf05b5
Aarna chauhan <chauhanaarna6@gmail.com>
2024-11-21 22:55:06 +0530

    Create buy and sell stocks.cpp

---
+++ Array/buy and sell stocks.cpp
@@ -0,0 +1,12 @@
+https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/stock-buy-and-sell2615
+int maximumProfit(vector<int> &prices) {
+        // code here
+        int n=prices.size();
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1]){
+                ans+=prices[i]-prices[i-1];
+            }
+        }
+        return ans;
+    }
