4d880f2afab81f7c0398881dde9755910f77c7e0
Aarna chauhan <chauhanaarna6@gmail.com>
2024-12-19 19:40:29 +0400

    Create no of occurrences .cpp

---
+++ Array/no of occurrences .cpp
@@ -0,0 +1,40 @@
+binary search- works only on sorted array tc- o(log n)
+find out last occurrence and first occurrence and then add 1 to it
+
+class Solution {
+  public:
+    int firstOcc(vector<int>& arr, int target){
+        int s=0,e=arr.size()-1;
+        int ans=-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                e=mid-1;
+            }
+            else if(arr[mid]>target) e=mid-1;
+            else s=mid+1;
+        }
+        return ans;
+    }
+    int lastOcc(vector<int>& arr, int target){
+        int s=0,e=arr.size()-1;
+        int ans=-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            if(arr[mid]==target){
+                ans=mid;
+                s=mid+1;
+            }
+            else if(arr[mid]>target) e=mid-1;
+            else s=mid+1;
+        }
+        return ans;
+    }
+    int countFreq(vector<int>& arr, int target) {
+        //CodeGenius
+        if(lastOcc(arr,target)==-1 && firstOcc(arr,target)==-1) return 0;
+        return lastOcc(arr,target)-firstOcc(arr,target) +1;
+        
+    }
+};
