23949c638673efa9a442afc5b1bb82b886a66637
Aarna chauhan <chauhanaarna6@gmail.com>
2024-12-22 01:34:23 +0400

    Update length of loop.cpp

--- Linkedlist /length of loop.cpp
+++ Linkedlist /length of loop.cpp
@@ -20,5 +20,55 @@ while(temp!=nullptr){
     timer++;
 }
 return 0;
-
 }
+
+optimal
+class Solution {
+  public:
+    // Function to find the length of a loop in the linked list.
+ int findlength(Node* slow,Node* fast){
+   int cnt=1;
+   fast=fast->next;
+   while(slow!=fast){
+    cnt++;
+    fast=fast->next;
+      
+   }
+   
+  return cnt;
+     
+ }   
+    
+  
+    
+    
+    int countNodesinLoop(Node *head) {
+        // Code here
+        Node* slow=head;
+        Node* fast=head;
+        while(fast!=nullptr&& fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if (slow==fast) return findlength(slow,fast);
+        }
+        return 0;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
