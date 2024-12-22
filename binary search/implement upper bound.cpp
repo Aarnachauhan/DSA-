94bebf94f339c8e23c7459500f360cf88fa3f979
Aarna chauhan <chauhanaarna6@gmail.com>
2024-12-22 14:17:28 +0400

    Create implement upper bound.cpp

---
+++ binary search/implement upper bound.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        // code here
+        int low=0;
+        int n=arr.size();
+        int high=n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            //maybe the answer
+            if(arr[mid]>target){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
