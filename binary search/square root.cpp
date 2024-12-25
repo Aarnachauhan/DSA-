7cbe18d024f10b39a74961bcd1ff114c5e9e8deb
Aarna chauhan <chauhanaarna6@gmail.com>
2024-12-25 22:14:38 +0400

    Create square root.cpp

---
+++ binary search/square root.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // Your code goes here
+        int s=0;
+        int e=n;
+        while(s<=e){
+            long long mid=(s+e)/2;
+      
+            if(mid<=n/mid){ //mid*mid can overflow
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
+        }
+        return e; //high will come to the max no and it will break while loop
+    }
+};
