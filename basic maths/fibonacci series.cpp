594e8a4b2591096b6de321a7ae1cd19a365b76be
Aarna chauhan <chauhanaarna6@gmail.com>
2025-01-11 15:56:26 +0400

    Create fibonacci series.cpp

---
+++ basic maths/fibonacci series.cpp
@@ -0,0 +1,17 @@
+lc 509
+tc-O(n)
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1) return n;
+        int a=0;
+        int b=1;
+
+        for(int i=2;i<=n;i++){
+            int temp=b;
+            b=a+b;
+            a=temp;
+        }
+        return b;
+    }
+}
