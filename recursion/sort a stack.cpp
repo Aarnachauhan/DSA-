34d13886c063e9e177bbc1ca1df72eb32b38a4d6
Aarna chauhan <chauhanaarna6@gmail.com>
2025-01-12 20:26:18 +0400

    Create sort a stack.cpp

---
+++ recursion/sort a stack.cpp
@@ -0,0 +1,25 @@
+void insert_correct_position(int x, stack<int> &s){
+ if(s.size()==0  || s.top()<x ){
+     s.push(x);
+ } else{
+     int a=s.top();
+     s.pop();
+     insert_correct_position(x,s);
+     s.push(a);
+ }   
+}
+void reverse(stack <int> &s){
+if(s.size()>0){
+    int x=s.top();
+    s.pop();
+    reverse(s);
+    insert_correct_position(x,s);
+}
+
+}
+
+void SortedStack :: sort()
+{
+   //Your code here
+   reverse(s);
+}
