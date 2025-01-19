043156bd620886fc446dc4e7c76146733d6aea56
Aarna chauhan <chauhanaarna6@gmail.com>
2025-01-19 19:11:18 +0530

    Create count subs sum k.cpp

---
+++ recursion/count subs sum k.cpp
@@ -0,0 +1,31 @@
+perfect sum problem 
+gfg
+this needs optimization using dp
+public:
+    int prints(int ind,int s,vector<int> &arr,int target,int n){
+        
+        if(s>target) return 0;
+        if(ind==n){
+            if(s==target){
+            return 1;
+        }else{
+            return 0;
+        }
+        }
+        s+=arr[ind];
+        int l=prints(ind+1,s,arr,target,n);
+        
+        s-=arr[ind];
+        int r=prints(ind +1 ,s,arr,target,n);
+        
+        return l+r;
+    }
+    int perfectSum(vector<int>& arr, int target) {
+        // code here
+        int ind;
+        int s;
+        int n=arr.size();
+        
+        return prints(0,0,arr,target,n);
+        
+    }
