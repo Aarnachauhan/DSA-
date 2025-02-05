405d1bcdad4a1323af31c64b533b23c8c7bfb0d0
Aarna chauhan <chauhanaarna6@gmail.com>
2025-02-05 20:21:59 +0530

    Create lemonade prob.cpp

---
+++ greedy algo/lemonade prob.cpp
@@ -0,0 +1,34 @@
+lc 860
+tc-o(n)
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n=bills.size();
+        int five=0;
+        int ten=0;
+        for(int i=0;i<n;i++){
+            if(bills[i]==5){
+                five++;
+            }
+            else if(bills[i]==10){
+                if(five){
+                    five--;
+                    ten++;
+                }else{
+                    return false;
+                }
+            }else{
+            if(five && ten){
+                five--;
+                ten--;
+            }else if(five>=3){
+                five-=3;
+            }else{
+                return false;
+            }
+        }
+        }
+        return true;
+    }
+};
