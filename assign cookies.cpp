f4ed82852a07b0018df10c7c057a3ae8113ee507
Aarna chauhan <chauhanaarna6@gmail.com>
2025-02-05 19:58:25 +0530

    Create assign cookies.cpp

---
+++ assign cookies.cpp
@@ -0,0 +1,24 @@
+lc 455
+tc- nlogn +mlogm +m
+as while loop is working on m 
+sc= o(1)
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int n=g.size();
+        int m=s.size();
+
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int l=0;
+        int r=0;
+        while(l<n && r<m){
+            if(g[l]<=s[r]){
+               l++;
+            }
+            r++;
+        }
+        return l;
+    }
+};
