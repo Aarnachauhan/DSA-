590381d7accf18b7a87908d8b18c40f66cf03695
Aarna chauhan <chauhanaarna6@gmail.com>
2025-02-14 10:35:33 +0530

    Create postorder traversal.cpp

---
+++ binary trees/postorder traversal.cpp
@@ -0,0 +1,19 @@
+lc 145
+
+class Solution {
+public:
+void postorder(TreeNode* root,vector<int> &res){
+    if(root==NULL) return;
+
+    postorder(root->left,res);
+    postorder(root->right,res);
+
+    res.push_back(root->val);
+    return;
+}
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        postorder(root,res);
+        return res;
+    }
+};
