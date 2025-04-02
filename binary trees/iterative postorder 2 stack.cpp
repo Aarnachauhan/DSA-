ae0de08d5d8f5ac83ea595b728dd418228f5e09d
Aarna chauhan <chauhanaarna6@gmail.com>
2025-04-02 20:02:54 +0530

    Create iterative postorder 2 stack.cpp

---
+++ binary trees/iterative postorder 2 stack.cpp
@@ -0,0 +1,32 @@
+lc 145
+
+tc-o(2n)
+sc-o(2n)
+
+  
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> postorder;
+        if(root==nullptr) return postorder;
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            root=st1.top();
+            st1.pop();
+
+            st2.push(root);
+
+            if(root->left!=nullptr) st1.push(root->left);
+            if(root->right!=nullptr) st1.push(root->right);
+
+        }
+
+     while(!st2.empty()){
+        postorder.push_back(st2.top()->val);
+        st2.pop();
+
+     }
+     return postorder;
+    }
+};
