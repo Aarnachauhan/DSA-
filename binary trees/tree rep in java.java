f6c59bdbe9b91a8b4e313bee2971dd8fdb1dc80d
Aarna chauhan <chauhanaarna6@gmail.com>
2025-04-07 18:27:35 +0530

    Update tree rep in java.java

--- binary trees/tree rep in java.java
+++ binary trees/tree rep in java.java
@@ -1,32 +1,33 @@
-/*struct node {
-  int data;
-  struct node *left;
-  struct node *right;
-};
+Understanding ArrayList and get()
+ArrayList:
 
-struct node *newNode(int data) {
-  struct node *node = (struct node *)malloc(sizeof(struct node));
+An ArrayList in Java is a resizable array implementation of the List interface. It allows you to store a dynamic list of elements, which can be accessed by their index.
+The indices in an ArrayList are zero-based, meaning that the first element is at index 0, the second element is at index 1, and so on.
+The get(int index) Method:
 
-  node->data = data;
+The get(int index) method is used to retrieve the element at the specified index from the ArrayList.
+For example:
+v.get(0) retrieves the first element.
+v.get(1) retrieves the second element.
+v.get(2) retrieves the third element, and so forth.
 
-  node->left = NULL;
-  node->right = NULL;
-  return (node);
-}*/
-
+//User function Template for Java
+// class Node{
+//     int data;
+//     Node left,right;
+//     Node(int n){
+//         data=n;
+//         left=right=null;
+//     }
+// }
 class Solution{
-public:
-
-    void create_tree(node* root0, vector<int> &vec){
-        //Your code goes here
-        node* root=root0;
-        root->left=newNode(vec[1]);
-        root->right=newNode(vec[2]);
-        root->left->left=newNode(vec[3]);
-        root->left->right=newNode(vec[4]);
-        
-        root->right->left=newNode(vec[5]);
-        root->right->right=newNode(vec[6]);
-        
-        
+    public static void createTree(Node root, ArrayList<Integer> v ){
+        //root= new Node(v.get(0));
+        root.left= new Node(v.get(1));
+        root.right= new Node(v.get(2));
+        root.left.left= new Node(v.get(3));
+        root.left.right= new Node(v.get(4));
+        root.right.left= new Node(v.get(5));
+        root.right.right= new Node(v.get(6));
     }
+}
