4722ad9cd7e3a1b76e5fac17fe1fbe5afa5484c7
Aarna chauhan <chauhanaarna6@gmail.com>
2025-05-28 14:30:38 +0400

    Update search in sorted rotated array ii.cpp

--- binary search/search in sorted rotated array ii.cpp
+++ binary search/search in sorted rotated array ii.cpp
@@ -4,29 +4,32 @@ public:
         int n=nums.size();
         int s=0;
         int e=n-1;
-        while(s<=e){
         int mid=(s+e)/2;
-        if(nums[mid]==target) return true;
-        //edge cases
-        if(nums[s]==nums[mid] && nums[mid]==nums[e]) {
-            s++;
-            e--;
-            continue;
+        while(s<=e){
+            if(nums[mid]==target) return true;
+            //edge cases
+             if(nums[s]==nums[mid] && nums[mid]==nums[e]){
+                s++;
+                e--;
+                continue;
+             }
+
+            if(nums[s]<=nums[mid]){
+                if(nums[s]<=target && nums[mid]>=target){
+                    e=mid-1;
+                }else{
+                    s=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && nums[e]>=target){
+                    s=mid+1;
+                }else{
+                    e=mid-1;
+                }
+            }
+            mid=(s+e)/2;
         }
-        if(nums[s]<=nums[mid]){ //left part is sorted
-           if(nums[s]<=target && target<=nums[mid]){
-            e=mid-1;
-           }else{
-            s=mid+1;
-           }
-        }else{ //right part is sorted
-          if(nums[mid]<=target && target<= nums[e]){
-            s=mid+1;
-          }else{
-            e=mid-1;
-          }
-        }
-        }
-       return false;
+        return false;
     }
 };
