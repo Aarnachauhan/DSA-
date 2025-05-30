d520888109be302cce9ffede28c5eabb2eadcada
Aarna chauhan <chauhanaarna6@gmail.com>
2025-05-30 11:06:22 +0400

    Update single element.cpp

--- binary search/single element.cpp
+++ binary search/single element.cpp
@@ -12,7 +12,7 @@ public:
         while(s<=e){
             int mid=(s+e)/2;
             if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1] ) return nums[mid];
-             //we are on the left and element is on the right (even,odd) (1,1)
+             //we are on the left and element is on the right (even,odd) (0,1)
              if(mid%2==1 && nums[mid-1]==nums[mid] || 
              mid%2==0 && nums[mid]==nums[mid+1] ){
                 s=mid+1;
