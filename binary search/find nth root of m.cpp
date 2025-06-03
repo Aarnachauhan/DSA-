a7547ba15bd215e378e940df5e0b951596e81eb4
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-03 17:02:46 +0400

    Update find nth root of m.cpp

--- binary search/find nth root of m.cpp
+++ binary search/find nth root of m.cpp
@@ -1,18 +1,32 @@
-class Solution {
-  public:
-    int floorSqrt(int n) {
-        // Your code goes here
-        int s=0;
-        int e=n;
-        while(s<=e){
-            long long mid=(s+e)/2;
-      
-            if(mid<=n/mid){ //mid*mid can overflow
-                s=mid+1;
-            }else{
-                e=mid-1;
-            }
-        }
-        return e; //high will come to the max no and it will break while loop
-    }
-};
+naukri.com
+tc- O(log2m)*log2n
+sc-O(1)
+
+int func(int n,int m , int mid){
+ long long ans=1;
+ for(int i=1;i<=n;i++){
+   ans=ans*mid;
+   if(ans>m) return 2;
+ }
+ if(ans==m) return 1;
+ else return 0;
+}
+
+//1 ->same
+//0 -> small
+//2 ->big
+
+
+int NthRoot(int n, int m) {
+  // Write your code here.
+  int s=1;
+  int h=m;
+  while(s<=h){
+    int mid=(s+h)/2;
+    int midN=func(n,m,mid);
+    if(midN==1) return mid;
+    else if(midN==2) h=mid-1;
+    else s=mid+1;
+  }
+  return -1;
+}
