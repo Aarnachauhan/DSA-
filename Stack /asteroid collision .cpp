8c81b423d5ea912645dc506c11732fb809c7c56b
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-14 12:20:52 +0400

    Update asteroid collision .cpp

--- Stack /asteroid collision .cpp
+++ Stack /asteroid collision .cpp
@@ -46,3 +46,33 @@ public:
 using vector :
 tc- o(n)
   sc-o(n)
+    class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+
+        for(int &a: asteroids){
+            //collision condition : a is -ve and last element of the vector is +ve
+            while(!st.empty() && a<0 && st.back()>0){
+                int sum=a+ st.back();
+
+                if(sum<0){ //+ve will explode 
+                    st.pop_back();
+                }
+                else if(sum>0){ //+ve will explode 
+                    a=0; //to break the while loop
+                }
+                else{ //sum=0
+                    st.pop_back(); 
+                    a=0;
+                }
+ 
+            }
+            if(a!=0){
+                st.push_back(a);
+            }
+        }
+
+       return st;
+    }
+};
