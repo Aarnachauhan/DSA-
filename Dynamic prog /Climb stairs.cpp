054bde3c0293909762dcf97d9b0faf3324326747
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-16 00:44:01 +0400

    Update Climb stairs.cpp

--- Dynamic prog /Climb stairs.cpp
+++ Dynamic prog /Climb stairs.cpp
@@ -19,3 +19,22 @@ mememorization :
 tc-o(n)
 sc-o(n)
   
+class Solution {
+public:
+    int t[46];
+int solve(int n, vector<int> &t){
+    if(n<0) return 0;
+    if(t[n]!=-1){
+        return t[n];
+    }
+    if(n==0) return 1;
+    int one=solve(n-1,t);
+    int two= solve(n-2,t);
+    return t[n] = one + two;
+}
+    int climbStairs(int n) {
+       vector<int> t(n+1,-1);
+       return solve(n,t);
+    }
+};
+
