a0cf0b548d2612859ea1de994b15e487924ae6fc
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-16 13:32:15 +0400

    Update and rename frog k jumps.cpp to Frog k jumps.cpp

--- Dynamic prog /Frog k jumps.cpp
+++ Dynamic prog /Frog k jumps.cpp
@@ -25,3 +25,23 @@ int main()
 }
 
 memorization
+tc-o(n*k)
+sc-o(n)
+
+#include <iostream>
+using namespace std;
+
+int dp[n];
+dp[0]=0;
+for(int i=1;i<=n;i++){
+    minsteps=INT_MAX;
+    for(int j=1;j<=k;j++){
+        if(i-j>0){
+             jump=dp[i-j] + abs(a[ind]- a[ind-j]);
+             minsteps=min(minsteps,jump);
+        }
+        dp[i]=minsteps;
+    }
+    return dp[n-1];
+}
+
