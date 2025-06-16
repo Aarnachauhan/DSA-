8761f3c956716dd6a869f16e94222460c69f959c
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-16 12:33:17 +0400

    Update Frog jump .cpp

--- Dynamic prog /Frog jump .cpp
+++ Dynamic prog /Frog jump .cpp
@@ -24,3 +24,25 @@ int frogJump(int n, vector<int> &heights)
     vector<int> dp(n+1 , -1);
     return f(n-1,heights,dp);
 }
+
+tabulation  --> bottom up
+tc=O(N)
+sc-o(N) + O(n)
+#include <bits/stdc++.h> 
+#include <iostream>
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    vector<int> dp(n,0);
+    dp[0]=0;
+    for(int i=1;i<n;i++){
+        int fs=dp[i-1] + abs(heights[i]-heights[i-1]);
+        int ss=INT_MAX;
+
+        if(i>1){
+            ss=dp[i-2] + abs(heights[i]-heights[i-2]);
+        }
+        dp[i]=min(fs,ss);
+    }
+   return dp[n-1];
+}
