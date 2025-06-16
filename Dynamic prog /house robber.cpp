a4df1ac0936fd165e0e20c730fee13a7a4f568b7
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-16 16:47:38 +0400

    Update house robber.cpp

--- Dynamic prog /house robber.cpp
+++ Dynamic prog /house robber.cpp
@@ -22,4 +22,27 @@ int f(int ind,vector<int> &nums,vector<int> &dp){
     }
 };
 
+space opt
+tx-o(n)
+sc-o(1)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int prev=nums[0];
+        int prev2=0;
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1){
+                take+=prev2;
+            }
+            int ntake=0 + prev;
+            int cur=max(take,ntake);
+            prev2=prev;
+            prev=cur;
+        }
+        return prev;
+    }
+};
+
 
