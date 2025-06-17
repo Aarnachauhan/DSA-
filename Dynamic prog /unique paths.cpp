8457f2c941577d5d2879928cd2b8f51f438fc257
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-17 16:10:09 +0400

    Update unique paths.cpp

--- Dynamic prog /unique paths.cpp
+++ Dynamic prog /unique paths.cpp
@@ -24,4 +24,23 @@ int rec(int i,int j, int m , int n){
 };
 
 memorization --->
-  
+tc-o(m*n)
+sc- o(path length ) + o(m*n)
+class Solution {
+public:
+int rec(int i,int j, int m , int n,vector<vector<int>>&dp){
+    if(i==0 && j==0) return 1;
+    if(i<0 || j<0) return 0;
+     if(dp[i][j]!=-1) return dp[i][j];
+    //move up 
+    int up=rec(i-1,j,m,n,dp);
+
+    //move left 
+    int left=rec(i,j-1,m,n,dp);
+    return dp[i][j]=up + left;
+}
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        return rec(m-1,n-1,m,n,dp);
+    }
+};
