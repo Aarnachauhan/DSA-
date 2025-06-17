bdcca416b50e41ccca7cb1e430a25d298fe98a59
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-17 16:24:59 +0400

    Update unique paths.cpp

--- Dynamic prog /unique paths.cpp
+++ Dynamic prog /unique paths.cpp
@@ -44,3 +44,30 @@ int rec(int i,int j, int m , int n,vector<vector<int>>&dp){
         return rec(m-1,n-1,m,n,dp);
     }
 };
+
+Tabulation --->
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[0][0]=1;
+                    continue;
+
+                } 
+                else{
+                    int up=0, left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up + left;
+                }
+            }
+           
+        }
+        return dp[m-1][n-1];
+        
+    }
+};
