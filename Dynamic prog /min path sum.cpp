fe26392701448ee84fb02916379d2022b9ef8ba0
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-19 12:45:04 +0400

    Update min path sum.cpp

--- Dynamic prog /min path sum.cpp
+++ Dynamic prog /min path sum.cpp
@@ -49,4 +49,29 @@ public:
 };
 
 tabulation
+class Solution {
+public:
+   int minPathSum(vector<vector<int>>& grid) {
+       int m=grid.size();
+       int n=grid[0].size();
+     
+      vector<vector<int>> dp(m,vector<int>(n,INT_MAX));
+
+      for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 && j==0){
+                dp[i][j] = grid[i][j];
+                continue;
+            }
+            int val= INT_MAX;
+            if(i-1>=0) val= min(val,dp[i-1][j]);
+            if(j-1>=0) val= min(val,dp[i][j-1]);
+
+            dp[i][j] = val+ grid[i][j];
+        }
+      }
+      return dp[m-1][n-1];
+    }
+};
+
 
