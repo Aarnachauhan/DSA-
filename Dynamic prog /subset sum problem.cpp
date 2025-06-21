8090aa6e519e75b5f05f269f7a009f43efad65c3
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-21 13:18:45 +0400

    Create subset sum problem.cpp

---
+++ Dynamic prog /subset sum problem.cpp
@@ -0,0 +1,32 @@
+gfg
+subset sum problem 
+
+memorization 
+class Solution {
+  public:
+  bool subsetsum(int ind,vector<vector<int>>  & dp , vector<int>&arr, int sum){
+      if(sum==0) return true;
+      if(ind==0) return arr[0]==sum ;
+      
+      if(dp[ind][sum]!=-1){
+          return dp[ind][sum];
+      }
+      
+      bool nottake= subsetsum(ind-1, dp , arr, sum);
+      
+      bool take= false;
+      if(arr[ind]<=sum){
+      take =subsetsum(ind-1 , dp , arr, sum-arr[ind]);
+      }
+      
+      return dp[ind][sum] = nottake || take;
+      
+      
+  }
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        int n=arr.size();
+        vector<vector<int>> dp(n,vector<int>(sum+1 , -1));
+        return subsetsum(n-1, dp , arr , sum );
+    }
+};
