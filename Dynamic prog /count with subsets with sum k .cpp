f708131346b8ffdd62908745e1527d0cf1c9a2fc
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-22 11:16:01 +0400

    Update count with subsets with sum k .cpp

--- Dynamic prog /count with subsets with sum k .cpp
+++ Dynamic prog /count with subsets with sum k .cpp
@@ -32,3 +32,35 @@ vector<vector<int>>&dp , int n){
         return countsubset(0,0,target,arr,dp, n);
     }
 };
+
+tabulation 
+tc- o(n*target)
+sc- o(n* target)
+
+class Solution {
+  public:
+   
+
+    int perfectSum(vector<int>& arr, int target) {
+        // code here
+        int n=arr.size();
+        
+        vector<vector<int>> dp(n+1, vector<int> (target + 1 , 0));
+        
+      dp[0][0] = 1;
+      
+      for(int i=1;i<=n;i++){
+          for(int j=0;j<=target; j++){
+              
+             //not take
+             dp[i][j]= dp[i-1][j];
+             
+             //take
+             if(j>=arr[i-1]){
+                  dp[i][j] += dp[i - 1][j - arr[i - 1]];
+             }
+          }
+      }
+      return dp[n][target];
+    }
+};
