95b8a5725da6091164798fdbbeebfd7d1283585a
Aarna chauhan <chauhanaarna6@gmail.com>
2025-06-27 15:13:29 +0400

    Update max cons one III.cpp

--- sliding window/max cons one III.cpp
+++ sliding window/max cons one III.cpp
@@ -34,4 +34,26 @@ better
 tc-o(2n)
 sc-o(1)
 
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int maxlen=0,l=0,r=0,zero=0,len=0;
+        while(r<nums.size()){
+            if(nums[r]==0) zero++;
+            while(zero>k){
+            if(nums[l]==0) zero--;
+            l++;
+            }
+            if(zero<=k){
+                len=r-l+1;
+                maxlen=max(maxlen,len);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
 
+optimal 
+tc-o(n)
+sc-o(1)
