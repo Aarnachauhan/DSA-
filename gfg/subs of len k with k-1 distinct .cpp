70f134ba4bca2659277627c8a7ca47727d8f1cf1
Aarna chauhan <chauhanaarna6@gmail.com>
2025-07-01 16:18:19 +0400

    Update subs of len k with k-1 distinct .cpp

--- gfg/subs of len k with k-1 distinct .cpp
+++ gfg/subs of len k with k-1 distinct .cpp
@@ -1,1 +1,31 @@
-
+tc-o(n)
+sc-o(1)
+class Solution {
+  public:
+    int substrCount(string &s, int k) {
+        // code here
+        int n=s.length();
+        if(k>n) return 0;
+        
+        unordered_map<char,int>freq;
+        int cnt=0;
+        int l=0;
+        
+        for(int r=0;r<n;r++){
+            freq[s[r]]++;
+            if(r-l+1>k){
+                freq[s[l]]--;
+                if(freq[s[l]]==0) freq.erase(s[l]);
+                l++;
+            }
+            
+            if(r-l+1==k){
+                if(freq.size()==k-1){
+                    cnt++;
+                }
+            }
+            
+        }
+        return cnt;
+    }
+};
