581fcb697de36d185b4dede78eedbdf9af4ecd1b
Aarna chauhan <chauhanaarna6@gmail.com>
2025-07-01 21:29:07 +0400

    Update longest harmonious subs.cpp

--- sliding window/longest harmonious subs.cpp
+++ sliding window/longest harmonious subs.cpp
@@ -18,3 +18,26 @@ public:
        return maxi;
     }
 };
+
+tc-o(N)
+sc-o(n)
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+     unordered_map<int,int> freq;
+
+     for(int num:nums){
+        freq[num]++;
+     }
+     int maxi=0;
+
+     for(auto &[num,count]:freq){
+        if(freq.count(num+1)){
+            int curr=count+ freq[num+1];
+            maxi=max(maxi,curr);
+        }
+     }
+     return maxi;
+    }
+
+};
