643359f4a46d8a1aeeb624cbada40015dadcfc4e
Aarna chauhan <chauhanaarna6@gmail.com>
2025-07-03 15:56:01 +0400

    Update longest substr wot repeating char.cpp

--- sliding window/longest substr wot repeating char.cpp
+++ sliding window/longest substr wot repeating char.cpp
@@ -22,4 +22,27 @@ public:
     }
 };
 
-using sliding window
+using sliding window (hashmap)
+tc-o(n)
+sc-o(3) = o(1)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n=s.length();
+        int maxlen=0;
+        int left=0;
+        unordered_map<char,int> count;
+        for(int r=0;r<n;r++){
+            char c=s[r];
+            count[c]++; //adding it to the hashmap
+
+            while(count[c]>1){
+                char leftchar=s[left];
+                count[leftchar]--;
+                left++;
+            }
+            maxlen=max(maxlen, r-left+1);
+        }
+        return maxlen;
+    }
+};
