96384cc317f6799fa169c8ecf4e19d863e7fa5ab
Aarna chauhan <chauhanaarna6@gmail.com>
2025-07-07 20:38:24 +0400

    Update max no of events.cpp

--- POTD /max no of events.cpp
+++ POTD /max no of events.cpp
@@ -1,5 +1,26 @@
 leetcode 1353
+key concept:
+vector<vector<int>> events = {
+    {1, 2},  // Event 1: can be attended on day 1 or 2
+    {2, 3},  // Event 2: can be attended on day 2 or 3
+    {3, 4}   // Event 3: can be attended on day 3 or 4
+};
+Here:
 
+events[0][0] = 1, events[0][1] = 2
+
+events[1][0] = 2, events[1][1] = 3
+
+events[2][0] = 3, events[2][1] = 4
+
+So:
+
+events[i][0] → start day of event i
+
+events[i][1] → end day of event i
+
+
+-----------------------------------------------
 class Solution {
 public:
     int maxEvents(vector<vector<int>>& events) {
