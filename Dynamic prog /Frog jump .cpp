808d51a0b99bd92eb9ece1490f0a62b4e76455e4
Aarna chauhan <chauhanaarna6@gmail.com>
2025-07-08 21:59:43 +0400

    Update Frog jump .cpp

--- Dynamic prog /Frog jump .cpp
+++ Dynamic prog /Frog jump .cpp
@@ -8,7 +8,7 @@ int f(int ind, vector<int> &heights , vector<int> &dp){
     if(dp[ind]!=-1) return dp[ind];
    
     int left=f(ind-1,heights,dp) + abs(heights[ind]- heights[ind-1]);
-    int right= INT_MAX;
+    int right= INT_MAX; //if there is no right then it will be int_max so we cant choose that path
     if(ind>1) {
         right=f(ind-2,heights,dp) + abs(heights[ind]- heights[ind-2]);
     }
