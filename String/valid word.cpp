58a7829a2578c3bec102e3c3c6d14b95bf8f776b
Aarna chauhan <chauhanaarna6@gmail.com>
2025-07-15 16:20:09 +0400

    Update valid word.cpp

--- String/valid word.cpp
+++ String/valid word.cpp
@@ -23,3 +23,27 @@ public:
         return v>=1 && con>=1;
     }
 };
+w/t built in function 
+class Solution {
+public:
+    bool isValid(string word) {
+        int n=word.length();
+        if(n<3) return false;
+        bool vowel=false , con=false;
+        for(int i=0;i<n;i++){
+            if(word[i]=='@' || word[i]=='#' || word[i]=='$') {
+                return false;
+            }
+            if(word[i]=='a' || word[i]=='e' || word[i]=='i' || word[i]=='o'
+            || word[i]=='u' || word[i]=='A' || word[i]=='E' || word[i]=='I' || word[i]=='O' || word[i]=='U'){
+                vowel=true;
+            }
+        else if( (word[i]>='a' && word[i]<='z') || (word[i]>='A' && word[i]<='Z')) 
+               con = true;
+        }
+                    
+        if(vowel == true && con == true) return true;
+        return false;  
+        }
+    
+};
