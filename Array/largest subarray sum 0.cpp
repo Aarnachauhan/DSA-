644e6ac07765cd51e146b23637b6897f65140409
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-16 17:20:28 +0530

    Update largest subarray sum 0.cpp

--- Array/largest subarray sum 0.cpp
+++ Array/largest subarray sum 0.cpp
@@ -1,3 +1,27 @@
+gfg
+naive- o(n2) 
+sc-O(1)
+class Solution {
+  public:
+    int maxLength(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        int maxi=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=0;
+            for(int j=i;j<n;j++){
+                sum+=arr[j];
+                if(sum==0){
+                  maxi=max(maxi,j-i+1);  
+                }
+            }
+        }
+        return maxi;
+    }
+};
+
+
 //tc-o(n logn) where n is for traversal , logn n for hashmap traversal
 //sc- O(n)
 gfg
