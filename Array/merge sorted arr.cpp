117dfd9ccbf890330b758cc43a3a24bec4e90ad1
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-17 19:57:44 +0530

    Update merge sorted arr.cpp

--- Array/merge sorted arr.cpp
+++ Array/merge sorted arr.cpp
@@ -1,14 +1,22 @@
+leetcode 88
+tc-o(m+n)
+sc-o(1)
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        //int n=nums1.size();
-        int i=0;
-        while(i<n){
-          if(nums1[i]==0){
-            nums1.push_back(nums2[i]);
-            i++;
-          }
-        }
-        sort(nums1.begin(),nums1.end());
+       int i=m-1;
+       int j=n-1;
+
+       int r=m+n-1;
+       while(j>=0){ //-1 aane per khatam loop
+        if(i>=0 &&nums1[i]>nums2[j]){
+          nums1[r]=nums1[i];
+          i--;
+       }else{
+        nums1[r]=nums2[j];
+        j--;
+       }
+       r--;
+    }
     }
 };
