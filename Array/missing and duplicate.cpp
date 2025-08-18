9ea3cb399630adadc895f86973a477cdd6b6800c
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-18 14:56:28 +0530

    Update missing and duplicate.cpp

--- Array/missing and duplicate.cpp
+++ Array/missing and duplicate.cpp
@@ -25,3 +25,29 @@ class Solution {
 optimal 
 tc-o(n)
 sc-o(1)
+class Solution {
+  public:
+    vector<int> findTwoElement(vector<int>& arr) {
+        // code here
+        long long n=arr.size();
+        
+        long long sn=(n*(n+1))/2; //x-y
+        long long s2n=(n*(n+1)*(2*n+1))/6; //x2-y2
+        
+        long long s=0, s2=0;
+        for(int i=0;i<n;i++){
+            s+=arr[i];
+            s2+=(long long)arr[i]*(long long)arr[i];
+        }
+        long long val1=s-sn;
+        long long val2=s2-s2n;
+        val2=val2/val1;
+        long long x=(val1+val2)/2;
+        long long y=x-val1;
+        
+        return {(int)x,(int)y};
+        
+        }
+};
+
+optimal 2- array marking
