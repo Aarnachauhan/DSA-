fa0a5fafd3ca04f16881f0e4ee3fbea842194797
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-18 15:41:41 +0530

    Update count inversions of an array.cpp

--- Array/count inversions of an array.cpp
+++ Array/count inversions of an array.cpp
@@ -1,1 +1,23 @@
 gfg
+
+brute force- code written by me ! hehhehe
+  tle  1110/1115
+  class Solution {
+  public:
+    int inversionCount(vector<int> &arr) {
+        // Code Here
+        int cnt=0;
+        int n=arr.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j] && i<j){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
+
+
+optimal
