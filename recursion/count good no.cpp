655c1d9922c3579e5d44774cd976ec4f0eabeffa
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-19 19:49:02 +0530

    Update count good no.cpp

--- recursion/count good no.cpp
+++ recursion/count good no.cpp
@@ -1,35 +1,20 @@
-Time complexity: O(log N)
-Memory complexity: O(log N) because a function call consumes memory and log N recursive function calls are made
+Time complexity: O(log b)
+Memory complexity: O(log b) because a function call consumes memory and log b recursive function calls are made
 
-
-#define ll long long
 class Solution {
 public:
-    // evens  = 0, 2, 4, 6, 8  => 5 evens
-    // primes = 2, 3, 5, 7     => 4 primes
-  
-   int p=1e9 + 7; //mod
+   const int M=1e9+7;
+   int findpower(long long a, long long b){
+    if(b==0) return 1; //base case
+    long long half=findpower(a,b/2);
+    long long result=(half*half)%M;
 
-long long power(long long x,long long y){
-    long long res=1; //result
+    if(b%2==1) result= (result*a) %M;
 
-    x=x%p; //if x is greater than or equal to p
-    if(x==0) return 0;
+    return result;
 
-    while(y>0){
-        //y bitwise to 1 is 1 in case of odd and 0 in case of even
-        if(y&1) res=(res*x) % p; //for odd
-         //for even index
-        y=y>>1; //y>>1 is same as y/2
-        x=(x*x) % p;
-    }
-    return res;
-}
-
+   }
     int countGoodNumbers(long long n) {
-        long long odd=n/2; //odd no of indexs
-        long long even=n-odd; //even no of indexs
-        long long ans=((power(4LL,odd) %p * power(5LL,even) %p)%p);
-        return (int)ans;
+        return (long long)findpower(5,(n+1)/2) * findpower(4,n/2) % M;
     }
 };
