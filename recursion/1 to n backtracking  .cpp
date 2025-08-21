9b6d0e5e880c0bea2b780b3794cc60fbd202011c
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-21 17:05:17 +0530

    Update 1 to n backtracking  .cpp

--- recursion/1 to n backtracking  .cpp
+++ recursion/1 to n backtracking  .cpp
@@ -1,7 +1,19 @@
-gfg backtracking
-public:
-    // Complete this function
-    if(n==0) return;
-        printNos(n-1);
-        cout<<n<<" ";
-    }
+codechef compiler
+#include <bits/stdc++.h>
+using namespace std;
+void recur(int n){
+    if(n==0) return ;
+    recur(n-1);
+    cout<<n<<endl;
+}
+
+
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	recur(n);
+
+}
+
