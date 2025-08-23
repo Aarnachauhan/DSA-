b15c4e12fc6e1020ff993a12255024424e0c6c37
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-24 00:19:40 +0530

    Update search in 2d matrix ii.cpp

--- binary search/search in 2d matrix ii.cpp
+++ binary search/search in 2d matrix ii.cpp
@@ -30,3 +30,21 @@ int bs(vector<int>& mat, int target){
     }
 };
 
+optimal 
+tc-o(n+m)
+sc-o(1)
+ class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        int row=0; int col=m-1;
+        while(row<n && col>=0){
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col] <target ) row++;
+            else col--;
+        }
+        return false;
+    }
+};
