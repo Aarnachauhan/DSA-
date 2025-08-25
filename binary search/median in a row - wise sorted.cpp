6379e2005153072667e4add2e48f355748653d86
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-25 19:38:11 +0530

    Update median in a row - wise sorted.cpp

--- binary search/median in a row - wise sorted.cpp
+++ binary search/median in a row - wise sorted.cpp
@@ -21,3 +21,47 @@ sc-o(n*m)
 };
 
 optimal 
+class Solution {
+  public:
+int upperbound(vector<int> &matrix , int x , int n){
+    int low=0 ; int high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(matrix[mid]>x){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+int countsmallelements(vector<vector<int>> &mat , int n , int m , int x){
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        cnt+=upperbound(mat[i],x,m);
+    }
+    return cnt;
+}
+    int median(vector<vector<int>> &mat) {
+        // code here
+        int low=INT_MAX ;
+        int high=INT_MIN;
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int i=0;i<n;i++){
+            low=min(low , mat[i][0]);
+            high=max(high, mat[i][m-1]);
+        }
+        int req=(n*m)/2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int smallequals=countsmallelements(mat,n,m,mid);
+            if(smallequals <=req) low=mid+1;
+            else high=mid-1;
+        }
+        return low;
+    }
+};
