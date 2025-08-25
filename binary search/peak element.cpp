f74f5b564c98da5295d69547fdd04b7614ec61e2
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-25 10:59:22 +0530

    Update peak element.cpp

--- binary search/peak element.cpp
+++ binary search/peak element.cpp
@@ -16,7 +16,22 @@ public:
 };
 optimal answer-
 tc-o(nlogn)
-
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        int l=0; int h=n-1;
+        while(l<h){
+            int mid=l+(h-l)/2;
+            if(nums[mid]<nums[mid+1]){
+                l=mid+1;
+            }
+            else h=mid;
+        }
+        return l;
+    }
+};
+2nd answer
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
