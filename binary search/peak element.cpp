26c2e80c5e6d84895fedbb042c562db6e42d5aad
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-25 10:44:19 +0530

    Update peak element.cpp

--- binary search/peak element.cpp
+++ binary search/peak element.cpp
@@ -1,3 +1,22 @@
+leetcode 162
+brute force- o(n) 
+sc-o(1)
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if((i==0 || nums[i-1]<nums[i] )&& 
+            (i==n-1 || nums[i]>nums[i+1])){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+optimal answer-
+tc-o(nlogn)
+
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
