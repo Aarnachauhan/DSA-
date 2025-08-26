6dbdd85c2f511b2a3012b527aeffca8c146331c7
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-26 10:33:30 +0530

    Update reverse pairs.cpp

--- Array/reverse pairs.cpp
+++ Array/reverse pairs.cpp
@@ -17,3 +17,55 @@ public:
 };
 
 optimal approach
+tc-o(2nlogn)
+sc-o(n) 
+see striver video and notes for differnet code 
+class Solution {
+public:
+int cnt=0;
+void merge(int low , int mid , int high, vector<int> & nums){
+    int n1=mid-low+1;
+    int n2=high-mid;
+
+    vector<int> l(n1) , r(n2);
+    for(int i=0;i<n1;i++){
+        l[i]=nums[low+i];
+    }
+    for(int i=0;i<n2;i++){
+        r[i]=nums[mid+1+i];
+    }
+    //count reverse pairs
+    int j=mid+1;
+    for(int i=low ; i<=mid; i++){
+        while(j<=high && (long long )nums[i]>2LL* nums[j]){
+            j++;
+        }
+        cnt+=(j-(mid+1));
+    }
+    // Merge two sorted halves
+        int i = 0;
+        j = 0;
+        int k = low;
+        while (i < n1 && j < n2) {
+            if (l[i] <= r[j]) {
+                nums[k++] = l[i++];
+            } else {
+                nums[k++] = r[j++];
+            }
+        }
+        while (i < n1) nums[k++] = l[i++];
+        while (j < n2) nums[k++] = r[j++];
+}
+void mergesort(int low , int high, vector<int> &nums){
+    if(low>=high) return;
+    int mid=(low+high)/2;
+    mergesort(low,mid,nums);
+    mergesort(mid+1,high,nums);
+    merge(low,mid,high,nums);
+}
+    int reversePairs(vector<int>& nums) {
+        int n=nums.size();
+        mergesort(0,n-1,nums);
+        return cnt;
+    }
+};
