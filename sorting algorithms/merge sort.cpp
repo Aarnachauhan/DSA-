5b8d21e4248eb37e9bfac1377676e4672f85b1a5
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-26 11:59:57 +0530

    Create merge sort.cpp

---
+++ sorting algorithms/merge sort.cpp
@@ -0,0 +1,10 @@
+gfg
+tc-n for merging 
+and logn for dividing 
+so tc-o(nlogn)
+sc-o(logn) //recursion stack of space 
+
+and if interview ask to ignore recursive stack space then sc-o(n) as we are using 2 vectors to store.
+  
+
+  
