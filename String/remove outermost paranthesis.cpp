542bd5085cf3276d8cc96e71c596acf6b7b7a842
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-28 21:18:16 +0530

    Update remove outermost paranthesis.cpp

--- String/remove outermost paranthesis.cpp
+++ String/remove outermost paranthesis.cpp
@@ -1,4 +1,33 @@
 leetcode 1021
+brute force 
+tc-o(n)
+sc-o(n)
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        stack<char> st;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                if(st.empty()){ // if stack is empty that means its an outer paranthesis.
+                    st.push('('); //we dont have to add it in answer string.
+                }else{
+                    ans+='('; //add in answer as it is not outerstring.
+                    st.push('(');
+                }
+            }
+            else if(s[i]==')'){
+                st.pop();
+                if(!st.empty()){
+                    ans+=')'; //add in answer as it is not outerstring.
+                }
+            }
+        }
+        return ans;
+    }
+};
+
 class Solution {
 public:
     string removeOuterParentheses(string s) {
