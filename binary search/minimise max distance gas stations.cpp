a7ae66c09baad02376aef1aaf2b78405fa531313
Aarna chauhan <chauhanaarna6@gmail.com>
2025-08-29 21:28:51 +0530

    Update minimise max distance gas stations.cpp

--- binary search/minimise max distance gas stations.cpp
+++ binary search/minimise max distance gas stations.cpp
@@ -52,3 +52,41 @@ double minimiseMaxDistance(vector<int> &arr, int k){
     }
     return pq.top().first;
 }
+
+optimial - 
+tc-o(nlogn + n)
+sc-o()
+
+int noofgasstation(long double dist , vector<int> &arr){
+    int cnt=0;
+    for(int i=1;i<arr.size();i++){
+        int noinbetween=((arr[i]-arr[i-1])/dist);
+        if((arr[i]-arr[i-1])/dist==noinbetween*dist){
+            noinbetween--;
+        }
+        cnt+=noinbetween;
+    }
+    return cnt;
+}
+
+double minimiseMaxDistance(vector<int> &arr, int k){
+	// Write your code here.
+    int n=arr.size();
+    long double low=0 , high=0;
+    for(int i=0;i<n-1;i++){ //o(n)
+      high=max(high ,(long double) arr[i+1]-arr[i]);
+    }	
+    long double diff=1e-6;
+    while(high-low>diff){ //o(nlogn)
+        long double mid=(low+high)/(2.0);
+        int cnt=noofgasstation(mid,arr);
+        if(cnt>k){
+            low=mid;
+        }
+        else{
+            high=mid;
+        }
+    }
+    return high;
+}
+
