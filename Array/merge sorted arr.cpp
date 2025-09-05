b80c3c20bf0ca74b21cc619ffa64f37859ff963f
Aarna chauhan <chauhanaarna6@gmail.com>
2025-09-05 16:40:10 +0530

    Update merge sorted arr.cpp

--- Array/merge sorted arr.cpp
+++ Array/merge sorted arr.cpp
@@ -1,4 +1,18 @@
 leetcode 88
+brute force :
+tc-o((n+m) log(n+m))
+sc-o(1)
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+       for(int j=0 , i=m;j<n;j++){
+        nums1[i]=nums2[j];
+        i++;
+       } 
+       sort(nums1.begin(),nums1.end());
+    }
+};
+-------------------------------------------------------------------------------------------------------------------------------------------
 tc-o(m+n)
 sc-o(1)
 class Solution {
