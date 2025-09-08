b3f8cf8fec565cea5d897eafeb5f27a3edd75e1e
Aarna chauhan <chauhanaarna6@gmail.com>
2025-09-08 12:40:24 +0530

    Update defuse the bomb.cpp

--- sliding window/defuse the bomb.cpp
+++ sliding window/defuse the bomb.cpp
@@ -27,5 +27,46 @@ public:
 sliding window 
 tc-o(n)
 sc-o(n) 
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int N = code.size();
+        vector<int> res(N, 0);
+        if (k == 0) return res;
+
+        int step = abs(k);
+        int sum = 0;
+
+        // starting window: sum next k elements if k > 0
+        // or previous k elements if k < 0
+        if (k > 0) {
+            for (int i = 1; i <= k; i++) {
+                sum += code[i % N];
+            }
+            res[0] = sum;
+
+            for (int i = 1; i < N; i++) {
+                // remove outgoing, add incoming
+                sum -= code[i % N];
+                sum += code[(i + k) % N];
+                res[i] = sum;
+            }
+        } else {
+            for (int i = 1; i <= step; i++) {
+                sum += code[(N - i) % N];
+            }
+            res[0] = sum;
+
+            for (int i = 1; i < N; i++) {
+                // remove outgoing, add incoming
+                sum -= code[(i - 1 - step + N) % N];
+                sum += code[(i - 1 + N) % N];
+                res[i] = sum;
+            }
+        }
+
+        return res;
+    }
+};
 
   
