397e908351952c422e81b3b76d346909c7e79433
Aarna chauhan <chauhanaarna6@gmail.com>
2025-09-08 11:59:08 +0530

    Update contain dup ii.cpp

--- sliding window/contain dup ii.cpp
+++ sliding window/contain dup ii.cpp
@@ -34,4 +34,22 @@ public:
     }
 };
 
-sliding window
+optimal 
+tc-o(n)
+sc-o(k)
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+       unordered_map<int,int> mpp;
+       int n=nums.size();
+       for(int i=0;i<n;i++){
+          if(mpp.count(nums[i])) return true;
+          mpp[nums[i]]=i;
+
+          if(mpp.size()>k) {
+            mpp.erase(nums[i-k]);
+          }
+       }
+       return false;
+    }
+};
