3496c0a43d1068a3bfa1f6c5e8c6e71d9dd46f82
Aarna chauhan <chauhanaarna6@gmail.com>
2025-09-08 11:37:47 +0530

    Update contain dup ii.cpp

--- sliding window/contain dup ii.cpp
+++ sliding window/contain dup ii.cpp
@@ -1,5 +1,5 @@
 leetcode 219
-
+better solution
 tc-o(n)
 sc-o(n)
 class Solution {
@@ -13,6 +13,24 @@ public:
             mpp[nums[i]]=i;
         }
         return false;
+    }
+};
+
+brute force - time limit exceeded
+tc--o(n2)
+sc--o(1)
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+       int n=nums.size();
+       for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+             if(nums[j]==nums[i]){
+                if(j-i <=k) return true;
+             }
+        }
+       }
+       return false;
     }
 };
 
