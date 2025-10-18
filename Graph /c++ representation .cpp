3ee454207be5a2b9197a2dcf07084d13ab400a75
Aarna chauhan <chauhanaarna6@gmail.com>
2025-10-18 15:02:49 +0400

    Update c++ representation .cpp

--- Graph /c++ representation .cpp
+++ Graph /c++ representation .cpp
@@ -1,6 +1,6 @@
 method 1
-tc-o(n*m)
-
+sc-o(n*m)
+tc-o(n)
   #include <bits/stdc++.h>
 using namespace std;
 
@@ -24,3 +24,24 @@ int main() {
 
 
 method 2
+sc-(2*e)
+tc-o(n)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,m;
+	cin>>n>>m;
+	
+    vector<int> adj[n+1];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v); //on the u'th index store v
+        adj[v].push_back(u); //vice versa
+    }
+	
+	return 0;
+
+}
