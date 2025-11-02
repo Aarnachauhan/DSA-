13f25361583fce8f4bc8708323e0966e55ec75a6
Aarna chauhan <chauhanaarna6@gmail.com>
2025-11-03 00:28:22 +0530

    Update no of provinces.cpp

--- Graph /no of provinces.cpp
+++ Graph /no of provinces.cpp
@@ -5,17 +5,17 @@ sc-o(n)
 class Solution {
 public:
 void dfs(int node , vector<bool> &vis, vector<vector<int>> &isConnected){
-    vis[node]=true;
-    int n=isConnected.size();
+    vis[node]=true; //mark current node as visited
+    int n=isConnected.size(); //total no of cities
     for(int i=0;i<n;i++){
-        if(isConnected[node][i]==1 && !vis[i]){
+        if(isConnected[node][i]==1 && !vis[i]){ //if i is connected to node and not visited.
             dfs(i,vis,isConnected);
         }
     }
 }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<bool> vis(n, false);
+        vector<bool> vis(n, false); // all will  have false stored in it.
         int cnt=0;
 
         for(int i=0;i<n;i++){
