c7e5bd69d4a9204a03bd9227bd49707078e17c03
Aarna chauhan <chauhanaarna6@gmail.com>
2025-11-09 11:10:33 +0530

    Create summary ranges.cpp

---
+++ String/summary ranges.cpp
@@ -0,0 +1,31 @@
+
+leetcode 228
+
+
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        if(nums.empty()){
+            return res;
+        }
+        for(int i=0;i<nums.size();){
+            int start=nums[i];
+            int j=i;
+
+            while(j+1<nums.size() && nums[j+1]==nums[j]+1){
+                j++;
+            }
+
+            if(nums[j]==start){
+                res.push_back(to_string(start));
+            }
+            else{
+                res.push_back(to_string(start)+"->" + to_string(nums[j]));
+            }
+
+            i=j+1;
+        }
+        return res;
+    }
+};
