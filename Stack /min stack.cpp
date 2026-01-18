538667b2962b1223c87c4d3ceedd0af5d2a353cc
Aarna chauhan <chauhanaarna6@gmail.com>
2026-01-18 16:27:17 +0530

    Update min stack.cpp

--- Stack /min stack.cpp
+++ Stack /min stack.cpp
@@ -1,4 +1,46 @@
 lc 155
+better :
+tc-o(1)
+sc-o(2*n)
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    stack<pair<int,int>> st;
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push({val,val});
+        }else{
+            st.push({val,min(val,st.top().second)});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+        
+    }
+    
+    int top() {
+       return st.top().first;
+    }
+    
+    int getMin() {
+      return  st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+optimal
 tc- O(1)
 class MinStack {
 public:
