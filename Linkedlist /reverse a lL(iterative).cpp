0afde6efd3c87e13386564ec2a2aaa1cf0ce79e8
Aarna chauhan <chauhanaarna6@gmail.com>
2026-02-17 14:58:09 +0530

    Update reverse a lL(iterative).cpp

--- Linkedlist /reverse a lL(iterative).cpp
+++ Linkedlist /reverse a lL(iterative).cpp
@@ -1,17 +1,26 @@
 tc-o(n)
 sc-0(1)
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp=head;
         ListNode* prev=nullptr;
-        while(temp!=nullptr){
-            ListNode* front=temp->next;
-            temp->next=prev;
-            prev=temp;
-            temp=front;
-            
+        ListNode* curr=head;
+
+        while(curr!=nullptr){
+            ListNode* temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
         }
         return prev;
     }
