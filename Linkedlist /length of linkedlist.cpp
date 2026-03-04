9cfadc9fda226f81b7eaf549c0dd5ecde83ff6d8
Aarna chauhan <chauhanaarna6@gmail.com>
2026-03-04 12:55:48 +0530

    Update length of linkedlist.cpp

--- Linkedlist /length of linkedlist.cpp
+++ Linkedlist /length of linkedlist.cpp
@@ -1,28 +1,17 @@
 gfg
 
 class Solution {
-    
   public:
-  int findlength(Node* slow , Node* fast){
-      int cnt=1;
-      fast=fast->next;
-      while(slow!=fast){
-          cnt++;
-          fast=fast->next;
-          
-      }
-      return cnt;
-  }
-    int lengthOfLoop(Node *head) {
-        // code here
-        Node* slow=head;
-        Node* fast=head;
-        
-        while(fast!=nullptr && fast->next!=nullptr){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast) return findlength(slow,fast);
+    int getCount(Node* head) {
+        // Code here
+        Node* temp=head;
+        if(head==nullptr) return 0;
+        int cnt=0;
+        while(temp->next!=nullptr){
+            cnt++;
+            temp=temp->next;
+            
         }
-        return 0;
+        return cnt+1;
     }
 };
