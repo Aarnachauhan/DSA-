7b4cedd45debb6bb2d24684f442a89372cb46d52
Aarna chauhan <chauhanaarna6@gmail.com>
2026-03-04 15:01:47 +0530

    Create length of cycle in linked list.cpp

---
+++ Linkedlist /length of cycle in linked list.cpp
@@ -0,0 +1,29 @@
+gfg
+
+tc-o(n) sc-o(1)
+  
+class Solution {
+    
+  public:
+  int findlength(Node* slow , Node* fast){
+      int cnt=1;
+      fast=fast->next;
+      while(slow!=fast){
+          cnt++;
+          fast=fast->next;
+      }
+      return cnt;
+  }
+    int lengthOfLoop(Node *head) {
+        // code here
+        Node* slow=head;
+        Node* fast=head;
+        
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return findlength(slow,fast);
+        }
+        return 0;
+    }
+};
