43dfde51764ff72dfc436f9d6b975ee168bcf9c9
Aarna chauhan <chauhanaarna6@gmail.com>
2026-03-06 22:09:42 +0530

    Update sorted and rotated.cpp

--- Array/sorted and rotated.cpp
+++ Array/sorted and rotated.cpp
@@ -4,13 +4,12 @@ public:
     bool check(vector<int>& nums) {
         int cnt=0;
         int n=nums.size();
-        for(int i=1;i<=n-2;i++){
-            if(nums[i-1]<nums[i] && nums[i+1]<nums[i]) cnt++;
-
+        for(int i=1;i<n;i++){
+          if(nums[i-1]>nums[i]) cnt++;
         }
+        if(nums[n-1]>nums[0]) cnt++;
         if(cnt==0) return true;
 
-        if(cnt==1 && nums[0]>nums[n-1]) return true;
-        else return false;
+        return cnt<=1;
     }
 };
