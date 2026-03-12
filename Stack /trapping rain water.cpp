6f9c01a655118ee33e36ea6d2a428d51153a73fc
Aarna chauhan <chauhanaarna6@gmail.com>
2026-03-12 11:35:04 +0530

    Update trapping rain water.cpp

--- Stack /trapping rain water.cpp
+++ Stack /trapping rain water.cpp
@@ -32,4 +32,35 @@ public:
 optimal :
 tc-o(n)
 sc-o(1)
-  
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int left=0,right=n-1;
+        int leftmax=0 , rightmax=0;
+        int water=0;
+
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>=leftmax){
+                    leftmax=height[left];
+                }
+                else{
+                    water+=leftmax-height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right]>=rightmax){
+                    rightmax=height[right];
+                }
+                else{
+                    water+=rightmax-height[right];
+                }
+                right--;
+            }
+            
+        }
+        return water;
+    }
+};
