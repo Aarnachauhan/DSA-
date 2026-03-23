7d3e9b87a95c30d70e97391143f76e1bf45363d2
Aarna chauhan <chauhanaarna6@gmail.com>
2026-03-23 23:26:35 +0530

    Update zig-zag traversal.cpp

--- binary trees/zig-zag traversal.cpp
+++ binary trees/zig-zag traversal.cpp
@@ -34,3 +34,51 @@ public:
          return result;
     }
 };
+2nd solution
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(!root) return ans;
+        bool dir=0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> temp;
+            while(n){
+                TreeNode* frontNode=q.front();
+                q.pop();
+                temp.push_back(frontNode->val);
+                if(frontNode->left){
+                    q.push(frontNode->left);
+                }
+                if(frontNode->right){
+                    q.push(frontNode->right);
+                }
+                n--;
+            }
+            if(dir==0 && !temp.empty()){
+                ans.push_back(temp);
+                dir=1;
+            }
+            else if(dir==1 && !temp.empty()){
+                reverse(temp.begin(),temp.end());
+                ans.push_back(temp);
+                dir=0;
+            }
+        }
+        return ans;
+    }
+};
