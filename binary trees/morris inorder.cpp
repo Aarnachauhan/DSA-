8c6d381298d61a524d8453914c23ed1be0ac0ba4
Aarna chauhan <chauhanaarna6@gmail.com>
2026-04-04 23:18:51 +0530

    Create morris inorder.cpp

---
+++ binary trees/morris inorder.cpp
@@ -0,0 +1,31 @@
+lc 94
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        TreeNode* curr=root;
+        while(curr!=NULL){
+            if(curr->left==nullptr){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* prev=curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==nullptr){
+                    prev->right=curr; //thread 
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=nullptr; //cut the thread;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
+        }
+        return inorder;
+    }
+};
