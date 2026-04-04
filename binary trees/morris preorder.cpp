5ff5daf2cd16eac33007dc350fbad65f6204cd83
Aarna chauhan <chauhanaarna6@gmail.com>
2026-04-04 23:21:44 +0530

    Create morris preorder.cpp

---
+++ binary trees/morris preorder.cpp
@@ -0,0 +1,43 @@
+lc 94
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder;
+        TreeNode* curr=root;
+        while(curr!=NULL){
+            if(curr->left==nullptr){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode* prev=curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==nullptr){
+                    prev->right=curr; //thread 
+                     inorder.push_back(curr->val);
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=nullptr; //cut the thread;
+                   
+                    curr=curr->right;
+                }
+            }
+        }
+        return inorder;
+    }
+};
