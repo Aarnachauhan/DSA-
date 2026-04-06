ab107a120b77035a6223ca9d9d8c5e55e375d492
Aarna chauhan <chauhanaarna6@gmail.com>
2026-04-06 08:31:49 +0530

    Update kth smallest ele in BST.cpp

--- BST/kth smallest ele in BST.cpp
+++ BST/kth smallest ele in BST.cpp
@@ -15,4 +15,53 @@ void inorder(TreeNode* root, vector<int> &v){
     }
 };
 
-optimal
+optimal - morris + cnt
+tc-o(n)
+sc-o(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    int helper(TreeNode* root, int k, int& preOrder)
+    {
+        if(root == NULL) return -1;
+
+        if(root->left)
+        {
+            int leftAns = helper(root->left, k, preOrder);
+
+            if(leftAns != -1) return leftAns;
+        }
+
+        if(preOrder + 1 == k) return root->val;
+        
+        preOrder = preOrder + 1;
+
+        if(root->right)
+        {
+            int rightAns = helper(root->right, k, preOrder);
+
+            if(rightAns != -1) return rightAns;
+        }
+
+        return -1;
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        int preOrder = 0;
+
+        return helper(root, k , preOrder);
+    }
+};
