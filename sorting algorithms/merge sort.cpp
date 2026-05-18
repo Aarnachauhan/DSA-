a857e98aef3171de9489e92bdd7558e044979fe1
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-18 11:25:45 +0400

    Update merge sort.cpp

--- sorting algorithms/merge sort.cpp
+++ sorting algorithms/merge sort.cpp
@@ -7,53 +7,35 @@ sc-o(logn) //recursion stack of space
 and if interview ask to ignore recursive stack space then sc-o(n) as we are using 2 vectors to store.
   
 
-  class Solution {
+class Solution {
   public:
-  void merge(vector<int> &arr , int l , int mid , int r){
-      int n1=mid-l+1;
-      int n2= r-mid; //( right - (mid +1 ) -1) -> right - mid
-      vector<int> L(n1) , R(n2);
-      for(int i=0;i<n1;i++){
-          L[i]= arr[l+i];
-      }
-      for(int j=0;j<n2;j++){
-          R[j]=arr[mid+1+j];
-      }
-      int i=0 , j=0 , k=l;
-      
-      while(i<n1 && j < n2){
-          if(L[i]<=R[j]){
-              arr[k]=L[i];
-              i++;
+  void merge(vector<int> &arr,int l , int mid, int r){
+      vector<int> temp;
+      int left=l , right=mid+1;
+      while(left<=mid && right<=r){
+          if(arr[left]<=arr[right]){
+              temp.push_back(arr[left]);
+              left++;
           }
-          else {
-              arr[k]=R[j];
-              j++;
+          else{
+              temp.push_back(arr[right]);
+              right++;
           }
-          k++;
       }
       
-      while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-
-    // Copy the remaining elements of R[], 
-    // if there are any
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+      while(left<=mid) temp.push_back(arr[left++]);
+      while(right<=r)  temp.push_back(arr[right++]);
+      
+      for (int i = l; i <= r; i++)
+            arr[i] = temp[i - l];
   }
     void mergeSort(vector<int>& arr, int l, int r) {
         // code here
-        if(l>=r) return; // only one element is left.
-        int mid=(l+r)/2;
-        mergeSort(arr, l , mid);
-        mergeSort(arr, mid+1 , r);
-        merge(arr, l , mid , r);
+        if(l>=r) return;
+        int mid=l+(r-l)/2;
+        mergeSort(arr,l,mid);
+        mergeSort(arr,mid+1,r);
+        merge(arr,l,mid,r);
         
     }
 };
