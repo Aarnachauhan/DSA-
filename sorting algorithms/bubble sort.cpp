a83199faf1cd91e4e42af9b52cc218d1f9444495
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-18 09:57:17 +0400

    Update bubble sort.cpp

--- sorting algorithms/bubble sort.cpp
+++ sorting algorithms/bubble sort.cpp
@@ -1,6 +1,24 @@
 gfg
 tc-o(n2)
 Sc-o(1)
+brute force:
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]>arr[j]){
+                    swap(arr[i],arr[j]);
+                }
+            }
+            
+        }
+    }
+};
+optimal :
+
 class Solution {
   public:
     void bubbleSort(vector<int>& arr) {
