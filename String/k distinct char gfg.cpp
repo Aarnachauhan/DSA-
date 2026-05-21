e392e16991a479853b3400d2ba1a0020f38ee09c
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-21 18:37:54 +0400

    Update k distinct char gfg.cpp

--- String/k distinct char gfg.cpp
+++ String/k distinct char gfg.cpp
@@ -22,3 +22,35 @@ class Solution {
         return ans;
     }
 };
+
+optimal
+class Solution {
+  public:
+ int count(string & s , int k){
+     int n=s.length();
+     int ans=0;
+     vector<int> freq(26,0);
+     int dist=0;
+     int i=0;
+     for(int j=0;j<n;j++){
+         freq[s[j]-'a']++;
+         if(freq[s[j]-'a']==1) dist++;
+         
+         while(dist>k){
+             freq[s[i]-'a']--;
+             if(freq[s[i]-'a']==0) dist--;;
+             i++;
+         }
+         
+         ans+=j-i+1;
+     }
+     return ans;
+ }
+    int countSubstr(string& s, int k) {
+        // code here
+        int n=s.length();
+        int ans=0;
+        ans=count(s,k)-count(s,k-1);
+        return ans;
+    }
+};
