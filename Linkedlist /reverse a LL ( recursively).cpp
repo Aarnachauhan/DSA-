25244fedc44aff1e8430377d1d8a6d36a26845ce
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-22 17:59:46 +0400

    Create reverse a LL ( recursively).cpp

---
+++ Linkedlist /reverse a LL ( recursively).cpp
@@ -0,0 +1,14 @@
+lc 206 
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==nullptr || head->next==nullptr){
+            return head;
+        }
+        ListNode* newhead=reverseList(head->next);
+
+        head->next->next=head;
+        head->next=nullptr;
+        return newhead;
+    }
+};
