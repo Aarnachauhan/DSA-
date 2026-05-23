37af99e8cd236ab0be2ac75770f649b25c725ac8
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-23 13:23:20 +0400

    Update next greater element.cpp

--- Stack /next greater element.cpp
+++ Stack /next greater element.cpp
@@ -2,6 +2,34 @@
 leetcode 496
 tc-o(n+m)
 sc-o(n)
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int , int> mpp;
+        stack<int> st;
+        vector<int> arr;
+        int n=nums2.size();
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i]) st.pop();
+            if(st.empty()){
+                mpp[nums2[i]]=-1;
+            }
+            else{
+                mpp[nums2[i]]=st.top();
+            }
+            st.push(nums2[i]);
+        }
+
+        for(int x:nums1){
+           arr.push_back(mpp[x]);
+        }
+        return arr;
+    }
+
+};
+
+or 
+
 
 class Solution {
 public:
