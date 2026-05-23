875b291df16ffa0b4cea27eb3ed9d8880b373d72
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-23 12:14:27 +0400

    Update imple in linkedlist.cpp

--- Stack /imple in linkedlist.cpp
+++ Stack /imple in linkedlist.cpp
@@ -1,74 +1,80 @@
-#include <bits/stdc++.h>
-using namespace std;
+https://www.naukri.com/code360/problems/implement-stack-with-linked-list_630475?leftPanelTabValue=PROBLEM
 
-struct Node{
-    int data;
-    Node *next;
-    Node(int x){
-        data=x;
-        next=NULL;
-    }
-};
+/****************************************************************
 
-struct MyStack{
-    Node *head;
-    int sz;
-    MyStack(){
-        head=NULL;
-        sz=0;
-    }
-    
-    void push(int x){
-        Node *temp=new Node(x);
-        temp->next=head;
-        head=temp;
-        sz++;
-    }
-    
-    int pop(){
-        if(head==NULL){
-            cout<<"Stack is Empty"<<endl;
-            return INT_MAX;
-            
-        }
-        else{
-        int res=head->data;
-        Node *temp=head;
-        head=head->next;
-        delete(temp);
-        sz--;
-        return res;
-        }
-    }
-    
-    int peek(){
-        if(head==NULL){
-            cout<<"Stack is Empty"<<endl;
-            return INT_MAX;}
-            else{
-        return head->data;
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+            int data;
+            Node *next;
+            Node()
+            {
+                this->data = 0;
+                next = NULL;
             }
+            Node(int data)
+            {
+                this->data = data;
+                this->next = NULL;
+            }
+            Node(int data, Node* next)
+            {
+                this->data = data;
+                this->next = next;
+            }
+        };
+
+
+*****************************************************************/
+
+class Stack
+{
+    //Write your code here
+     Node* top=nullptr;
+     int size=0;
+public:
+    Stack()
+    {
+        //Write your code here
     }
-    
-    int size(){
-        return sz;
+
+    int getSize()
+    {
+        //Write your code here
+        return size;
     }
-    
-    bool isEmpty(){
-        return head==NULL;
+
+    bool isEmpty()
+    {
+        //Write your code here
+        return size==0 ? true: false;
+    }
+
+    void push(int data)
+    {
+        //Write your code here
+        Node* temp=new Node(data);
+        temp->next=top;
+        top=temp;
+        size++;
+    }
+
+    void pop()
+    {
+        //Write your code here
+        if(isEmpty()) return;
+        Node* temp=top;
+        top=top->next;
+        delete temp;
+        size--;
+    }
+
+    int getTop()
+    {
+        //Write your code here
+        if(isEmpty()) return -1;
+        return top->data;
     }
 };
-
-int main()
-{
-    MyStack s;
-    s.push(5);
-    s.push(10);
-    s.push(20);
-    cout<<s.pop()<<endl;
-    cout<<s.size()<<endl;
-    cout<<s.peek()<<endl;
-    cout<<s.isEmpty()<<endl;
-  
-    return 0; 
-}
