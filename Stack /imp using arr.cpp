c16004c5534dddcd8d09c2dbdd2538644b7f80dd
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-23 11:52:17 +0400

    Update imp using arr.cpp

--- Stack /imp using arr.cpp
+++ Stack /imp using arr.cpp
@@ -1,23 +1,110 @@
-void MyStack ::push(int x) {
-    // Your Code
-    if(top>=999){
-        cout<<"size exceeds\n";
-        return;
-    }
-    else{
-        top++;
-        arr[top]=x;
-    }
-}
+https://www.naukri.com/code360/problems/stack-implementation-using-array_3210209
 
-// Function to remove an item from top of the stack.
-int MyStack ::pop() {
-    // Your Code
-    if(top<0){
-        return -1;
-    }else{
-        int x=arr[top];
-        top--;
-        return x;
+
+class Stack {
+
+    
+
+public:
+
+    int Top = -1;
+
+    int Max = 0;
+
+    vector<int> st;
+
+    
+
+    Stack(int capacity) {
+
+        Max = capacity;
+
+        st.reserve(capacity);
+
     }
-}
+
+ 
+
+    void push(int num) {
+
+        if(Top < Max - 1){
+
+            Top++;
+
+            st.push_back(num);
+
+        }
+
+    }
+
+ 
+
+    int pop() {
+
+        if(Top == -1){
+
+            return -1;
+
+        }
+
+        else{
+
+            int n = st[Top];
+
+            Top--;
+
+            st.pop_back();
+
+            return n;
+
+        } 
+
+        
+
+    }
+
+    
+
+    int top() {
+
+        if(Top < 0){
+
+            return -1;
+
+        }
+
+        else return st[Top];
+
+    }
+
+    
+
+    int isEmpty() {
+
+        if(Top == -1){
+
+            return 1;
+
+        }else{
+
+            return 0;
+
+        }
+
+    }
+
+    
+
+    int isFull() {
+
+        if(Top == Max - 1)return 1;
+
+        else return 0;
+
+    }
+
+    
+
+};
+
+ 
