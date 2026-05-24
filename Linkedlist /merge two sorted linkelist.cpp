464212317cedc7a9af1d6ba899d0a6fe22468b73
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-24 23:00:02 +0400

    Create merge two sorted linkelist.cpp

---
+++ Linkedlist /merge two sorted linkelist.cpp
@@ -0,0 +1,19 @@
+lc 
+tc-O(n+m)
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==nullptr || list2==nullptr){
+            return list1==nullptr? list2 : list1; 
+        } 
+        if(list1->val<=list2->val){
+            list1->next=mergeTwoLists(list1->next,list2);
+            return list1;
+        }
+        else{
+            list2->next=mergeTwoLists(list2->next, list1);
+            return list2;
+        }
+    }
+};
