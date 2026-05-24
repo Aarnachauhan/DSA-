b106bf27ea672a1892c4845aa67d3b1d6963ca33
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-24 22:13:12 +0400

    Create sort linkedlist.cpp

---
+++ Linkedlist /sort linkedlist.cpp
@@ -0,0 +1,28 @@
+lc 148
+naive solution
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int> arr;
+        ListNode* temp=head;
+        while(temp!=nullptr){
+            arr.push_back(temp->val);
+            temp=temp->next;
+        }
+
+        sort(arr.begin(),arr.end());
+        temp=head;
+        int i=0;
+        while(temp!=nullptr && i<arr.size()){
+            temp->val=arr[i];
+            i++;
+            temp=temp->next;
+        }
+        return head;
+    }
+};
+
+optimal:
+
+
