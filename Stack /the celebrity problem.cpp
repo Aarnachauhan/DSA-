ef64ee7cb412d4e5b7cbca181929b7d680cd86ec
Aarna chauhan <chauhanaarna6@gmail.com>
2026-05-27 17:15:39 +0400

    Update the celebrity problem.cpp

--- Stack /the celebrity problem.cpp
+++ Stack /the celebrity problem.cpp
@@ -26,3 +26,38 @@ class Solution {
         return -1;
     }
 };
+
+better solution 
+tc-o(n) sc-O(n)
+class Solution {
+  public:
+    int celebrity(vector<vector<int>>& mat) {
+        // code here
+        int n=mat.size();
+        stack<int> st;
+        
+        for(int i=0;i<n;i++){
+            st.push(i);
+        }
+        while(st.size()>1){
+            int a=st.top();
+            st.pop();
+            
+            int b=st.top();
+            st.pop();
+            
+            if(mat[a][b]) st.push(b);
+            else st.push(a);
+        }
+        int c=st.top();
+        for(int i=0;i<n;i++){
+            if(i==c) continue;
+            //if celeb knows someone =false
+            //if someone doesnt know celeb=false
+            if(mat[c][i] || !mat[i][c]){ 
+                return -1;
+            }
+        }
+        return c;
+    }
+};
