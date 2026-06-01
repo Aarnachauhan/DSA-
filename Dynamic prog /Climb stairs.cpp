883c11c329886ab76a6d20bee2ea5924161456c9
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-01 11:45:39 +0400

    Update Climb stairs.cpp

--- Dynamic prog /Climb stairs.cpp
+++ Dynamic prog /Climb stairs.cpp
@@ -38,22 +38,35 @@ int solve(int n, vector<int> &t){
     }
 };
 
+my solution for memo
+class Solution {
+public:
+int solve(vector<int> &dp , int n){
+    if(n<=2) return n;
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=solve(dp,n-1) + solve(dp, n-2);
+}
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(dp,n);
+    }
+};
+
 tabulation 
 
 class Solution {
 public:
+
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-           int one=dp[i-1];
-           int two=dp[i-2];
-           dp[i]=one+two;
-           
+        if(n<=2) return n;
+        vector<int> dp(n+1);
+        
+        dp[1]=1; 
+        dp[2]=2; 
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+ dp[i-2];
         }
         return dp[n];
-
     }
 };
 
