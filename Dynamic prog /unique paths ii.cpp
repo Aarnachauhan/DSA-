48e125953541a5a097e58a81889ba9e614923b50
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-02 13:42:38 +0400

    Update unique paths ii.cpp

--- Dynamic prog /unique paths ii.cpp
+++ Dynamic prog /unique paths ii.cpp
@@ -3,44 +3,41 @@ leetcode path ii
 recursion
 class Solution {
 public:
-int rec(int i,int j, int m , int n, vector<vector<int>>&obstacleGrid){
-    if(i>=m || j>=n || obstacleGrid[i][j]==1 ) return 0;
-    if(i==0 && j==0) return 1;
+int f(int i, int j, vector<vector<int>> & a){
+    if(i>=0 && j>=0 && a[i][j]==1 ) return 0;
+    if(i==0 && j==0 ) return 1;
+    if(i<0 || j<0) return 0;
 
-    //up 
-    int up=rec(i-1,j,m,n,obstacleGrid);
-    //left
-    int left=rec(i,j-1,m,n,obstacleGrid);
+    int up=f(i-1,j,a);
+    int left=f(i,j-1,a);
 
-    return up+ left;
+    return up+left;
 }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        return rec(0,0,m,n,obstacleGrid);
+        return f(m-1,n-1, obstacleGrid);
     }
 };
 
 memorization
 class Solution {
 public:
-int rec(int i,int j, int m , int n, vector<vector<int>>&obstacleGrid,vector<vector<int>>&dp){
-    if(i>=0 || j>=0 || obstacleGrid[i][j]==1 ) return 0;
-    if(i==0 && j==0) return 1;
+int f(int i, int j, vector<vector<int>> & a , vector<vector<int>> & dp){
+    if(i>=0 && j>=0 && a[i][j]==1 ) return 0;
+    if(i==0 && j==0 ) return 1;
+    if(i<0 || j<0) return 0;
     if(dp[i][j]!=-1) return dp[i][j];
+    int up=f(i-1,j,a,dp);
+    int left=f(i,j-1,a,dp);
 
-    //up 
-    int up=rec(i-1,j,m,n,obstacleGrid,dp);
-    //left
-    int left=rec(i,j-1,m,n,obstacleGrid,dp);
-
-    return up+ left;
+    return dp[i][j]=up+left;
 }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        vector<vector<int>> dp(m, vector<int> (n,-1));
-        return rec(0,0,m,n,obstacleGrid,dp);
+        vector<vector<int>>dp(m,vector<int> (n,-1));
+        return f(m-1,n-1, obstacleGrid ,dp);
     }
 };
 
@@ -51,23 +48,28 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
-        
-        vector<vector<int>> dp(m, vector<int> (n,0));
-
+         vector<vector<int>> dp(m, vector<int>(n, 0));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(obstacleGrid[i][j]==1) dp[i][j]=0;
-                else if(i==0 && j==0) dp[i][j]=1;
-                else{
-                    int up=0;
-                    int left=0;
-                    if(i>0) up=dp[i-1][j];
-                   if(j>0) left= dp[i][j-1];
-                   dp[i][j]=up + left;
-                }
-            }   
+             if( i>=0 && j>=0 &&  obstacleGrid[i][j]==1) {
+                dp[i][j]=0;
+                continue;
+             }
+             if(i==0 && j==0) {
+                dp[i][j]=1;
+                continue;
+             }
+             
+            
+            int up=0 , left=0;
+
+            if(i>0) up=dp[i-1][j];
+            if(j>0) left=dp[i][j-1];
+
+            dp[i][j]=left+up;
+            }
         }
-       return dp[m-1][n-1];
+        return dp[m-1][n-1];
     }
 };
 
