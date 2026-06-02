a3acd55e8b548b4073808294c0dbe1545fcedfb8
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-02 13:59:19 +0400

    Update min path sum.cpp

--- Dynamic prog /min path sum.cpp
+++ Dynamic prog /min path sum.cpp
@@ -3,27 +3,26 @@ leetcode 64
 recursion 
 class Solution {
 public:
- int rec(int i,int j, int m , int n , vector<vector<int>> &grid){
-    if(i==0 && j==0) return grid[0][0];
-    if(i<0 || j<0) return INT_MAX;
+int f(int i , int j, vector<vector<int>> & a){
+ if(i==0 && j==0) return a[i][j];
+ if(i<0 || j<0) return 1e9;
+  
+  int up=a[i][j]+ f(i-1, j,a);
+  int left=a[i][j]+ f(i,j-1,a);
 
-    //up 
-    int up= grid[i][j] + rec(i-1,j,m,n,grid);
+ return min(left, up);
 
-    //left
-    int left=grid[i][j] + rec(i,j-1,m,n,grid);
-    return min(up , left);
- }
+}
     int minPathSum(vector<vector<int>>& grid) {
-       int m=grid.size();
-       int n=grid[0].size();
-
-       return rec(m-1,n-1,m,n,grid);
-        
+        int m=grid.size();
+        int n=grid[0].size();
+        return f(m-1,n-1, grid);
     }
 };
 
-memo
+
+
+memoization
 class Solution {
 public:
  int rec(int i,int j,  vector<vector<int>> &grid,vector<vector<int>>&dp){
@@ -47,6 +46,8 @@ public:
         
     }
 };
+
+
 
 tabulation
 class Solution {
