2d6739d8054c49dc083c6f95e6357b220e4a1337
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-06 14:32:33 +0400

    Update partition equal subset sum.cpp

--- Dynamic prog /partition equal subset sum.cpp
+++ Dynamic prog /partition equal subset sum.cpp
@@ -60,15 +60,39 @@ Tc-O(n*t)
 sc-o(n*t)
 
 
+class Solution {
+public:
 
+    bool canPartition(vector<int>& nums) {
+        int totalsum=accumulate(nums.begin(),nums.end(),0);
+        if(totalsum%2!=0) return false;
+        int sum=totalsum/2;
+         int n=nums.size();
+        vector<vector<bool>> dp(n,vector<bool> (sum+1,false));
+       
+        
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        if(nums[0]<=sum){
+          dp[0][nums[0]]=true;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=sum;j++){
+               int exclude=dp[i-1][j];
 
+                int include=false;
+                if(nums[i]<=j){
+                    include=dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=include || exclude;
+            }
+        }
+        return dp[n-1][sum];
+    }
+};
 
-
-
-
-
-
-
+Space optimization:
 
     
 class Solution {
