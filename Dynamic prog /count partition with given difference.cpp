6a3166fe047c658edbe49a4b97a5596daa38d80c
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-07 21:24:16 +0400

    Update count partition with given difference.cpp

--- Dynamic prog /count partition with given difference.cpp
+++ Dynamic prog /count partition with given difference.cpp
@@ -32,4 +32,78 @@ class Solution {
 };
 -------------------------------------------------------------------------
 memoization 
+tc-o(n*target)
+sc- O(N×Target)+O(N)
+class Solution {
+  public:
   
+ int f(int ind , vector<int> &arr, int target,vector<vector<int>> &dp){
+     if(ind==0){
+     if(target==0 && arr[0]==0) return 2;
+     if(target==0 || target==arr[0]) return 1;
+     return 0;
+     }
+     if(dp[ind][target]!=-1) return dp[ind][target];
+     int notpick=f(ind-1 , arr, target,dp);
+     int pick=0;
+     if(arr[ind]<=target){
+         pick=f(ind-1,arr,target-arr[ind],dp);
+     }
+     return dp[ind][target]=pick+ notpick;
+ }
+    int countPartitions(vector<int>& arr, int diff) {
+        // Code here
+        int n=arr.size();
+        int totalsum=accumulate(arr.begin(),arr.end(),0);
+        if(totalsum-diff <0 ||(totalsum-diff)%2!=0 ) return 0;
+        
+        int target=(totalsum-diff)/2;
+        vector<vector<int>> dp(n , vector<int> (target+1,-1));
+        return f(n-1,arr,target,dp);
+    }
+};
+----------------------------------------------------------------------
+Tabulation
+class Solution {
+  public:
+  
+
+    int countPartitions(vector<int>& arr, int diff) {
+        // Code here
+        int n=arr.size();
+        int totalsum=accumulate(arr.begin(),arr.end(),0);
+        if(totalsum-diff <0 ||(totalsum-diff)%2!=0 ) return 0;
+        
+        int target=(totalsum-diff)/2;
+        vector<vector<int>> dp(n , vector<int> (target+1,0));
+        //return f(n-1,arr,target,dp);
+        if(arr[0]==0){
+            dp[0][0]=2;
+        }
+        else{
+            dp[0][0]=1;
+        }
+        if(arr[0] != 0 && arr[0] <= target)
+        dp[0][arr[0]] = 1;
+        
+        
+        for(int i=1;i<n;i++){
+            for(int j=0; j<=target;j++){
+                int notpick=dp[i-1][j];
+                int pick=0;
+                if(arr[i]<=j){
+                    pick=dp[i-1][j-arr[i]];
+                }
+                dp[i][j]=pick+notpick;
+            }
+            
+        }
+        
+      return dp[n-1][target];  
+    }
+};
+
+-----------------------------------------------------------------
+space optimization 
+
+
