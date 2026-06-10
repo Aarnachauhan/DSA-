293389fb8dc24901eb6365068b1bf2e26fb08e7e
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-10 18:44:51 +0400

    Update distinct subsequences.cpp

--- Dynamic prog /distinct subsequences.cpp
+++ Dynamic prog /distinct subsequences.cpp
@@ -97,3 +97,32 @@ int numDistinct(string s, string t) {
        return (int)prev[m];
     }
 };
+
+
+space optimization - 1d
+    class Solution {
+public:
+int numDistinct(string s, string t) {
+         int n=s.length();
+        int m=t.length();
+        vector<unsigned long long> prev(m+1,0);
+       
+       prev[0]=1;
+       
+       for(int i=1;i<=n;i++){
+    
+        for(int j=m;j>=1;j--){
+            if(s[i-1]==t[j-1]){
+                prev[j]=prev[j-1] + prev[j]; 
+            }
+            else{
+                prev[j]=prev[j];
+            }
+        
+        }
+
+
+       }
+       return (int)prev[m];
+    }
+};
