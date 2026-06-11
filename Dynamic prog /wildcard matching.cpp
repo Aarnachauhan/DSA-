4bb4fd972ea51679d5045bc42a8be86c68f27b0a
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-11 15:15:31 +0400

    Update wildcard matching.cpp

--- Dynamic prog /wildcard matching.cpp
+++ Dynamic prog /wildcard matching.cpp
@@ -56,3 +56,107 @@ int f(int i , int j , string &s , string &t, vector<vector<int>> &dp){
 };
 
 tabulation:
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+
+        int n = s.size();
+        int m = p.size();
+
+        vector<vector<bool>> dp(
+            n+1, vector<bool>(m+1,false));
+
+        dp[0][0] = true;
+
+        for(int j=1;j<=m;j++){
+            bool flag=true;
+
+            for(int k=1;k<=j;k++){
+                if(p[k-1]!='*'){
+                    flag=false;
+                    break;
+                }
+            }
+
+            dp[0][j]=flag;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+
+                if(s[i-1]==p[j-1] ||
+                   p[j-1]=='?'){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+
+                else if(p[j-1]=='*'){
+                    dp[i][j]=
+                    dp[i-1][j] ||
+                    dp[i][j-1];
+                }
+
+                else{
+                    dp[i][j]=false;
+                }
+            }
+        }
+
+        return dp[n][m];
+    }
+};
+
+
+space optimization
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+
+        int n = s.size();
+        int m = p.size();
+
+        vector<bool> prev(m+1,false),
+                     curr(m+1,false);
+
+        prev[0]=true;
+
+        for(int j=1;j<=m;j++){
+
+            bool flag=true;
+
+            for(int k=1;k<=j;k++){
+                if(p[k-1]!='*'){
+                    flag=false;
+                    break;
+                }
+            }
+
+            prev[j]=flag;
+        }
+
+        for(int i=1;i<=n;i++){
+
+            curr[0]=false;
+
+            for(int j=1;j<=m;j++){
+
+                if(s[i-1]==p[j-1] ||
+                   p[j-1]=='?'){
+                    curr[j]=prev[j-1];
+                }
+
+                else if(p[j-1]=='*'){
+                    curr[j]=prev[j] ||
+                            curr[j-1];
+                }
+
+                else{
+                    curr[j]=false;
+                }
+            }
+
+            prev=curr;
+        }
+
+        return prev[m];
+    }
+};
