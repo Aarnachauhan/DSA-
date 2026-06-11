5ed1609fff2852157fba9785914dfab193eecdc6
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-11 18:36:41 +0400

    Update buy and sell stocks diff day.cpp

--- Array/buy and sell stocks diff day.cpp
+++ Array/buy and sell stocks diff day.cpp
@@ -2,15 +2,16 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int buy=prices[0];
-        int profit=0;
         int n=prices.size();
-        for(int i=1;i<n;i++){
-            if(prices[i]<buy) buy=prices[i];
-            else if(prices[i]-buy>profit){
-                profit=prices[i]-buy;
-            }
+       int mini=prices[0];
+       int profit=0;
+
+       for(int i=1;i<prices.size();i++){
+        if(mini>prices[i]){
+            mini=prices[i];
         }
-        return profit;
+        profit=max(profit,prices[i]-mini);
+       }
+       return profit;
     }
 };
