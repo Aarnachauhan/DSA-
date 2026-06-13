a70d0d75e4f7e1322a63cc9427dbe8ec89c5d9c5
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-13 17:25:10 +0400

    Update longest increasing subs.cpp

--- Dynamic prog /longest increasing subs.cpp
+++ Dynamic prog /longest increasing subs.cpp
@@ -1,32 +1,24 @@
 lc 300
 best solution :
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+tc-o(n2)
+sc-o(n)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+       int n=nums.size();
+       int maxi=1;
+       vector<int> dp(n,1);
+       for(int i=0;i<n;i++){
+        for(int prev=0;prev<i; prev++){
+            if(nums[prev]<nums[i]){
+                dp[i]=max(dp[i],1+dp[prev]);
+            }
+        }
+        maxi=max(maxi,dp[i]);
+       }
+       return maxi;
+    }
+};
 
 
 
