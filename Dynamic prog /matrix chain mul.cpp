f34ec4a9f2e954a61db9193537c2fde043fa6ab3
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-15 16:00:02 +0400

    Update matrix chain mul.cpp

--- Dynamic prog /matrix chain mul.cpp
+++ Dynamic prog /matrix chain mul.cpp
@@ -47,3 +47,27 @@ int f(int i , int j , vector<int> &arr, vector<vector<int>> &dp){
         return f(1,n-1,arr,dp);
     }
 };
+tabulation - giving runtime error , idk why
+class Solution {
+public:
+    int matrixMultiplication(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        for(int i=0;i<n;i++) dp[i][i]=0; //no need to write this
+        for(int i=n-1;i>=1;i++){
+            for(int j=i+1;j<n;j++){
+                int mini=1e9;
+    
+        for(int k=i;k<j;k++){
+        int steps= arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+        
+        mini = min(mini, steps);
+        }
+          dp[i][j]=mini;
+            }
+        }
+        return dp[1][n-1];
+    }
+};
