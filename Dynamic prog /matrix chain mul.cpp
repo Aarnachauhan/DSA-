1780143137ded48933c2b165171a8b6b6df68bcb
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-15 12:40:42 +0400

    Create matrix chain mul.cpp

---
+++ Dynamic prog /matrix chain mul.cpp
@@ -0,0 +1,49 @@
+gfg
+
+
+recursion
+class Solution {
+public:
+int f(int i , int j , vector<int> &arr){
+    if(i==j) return 0;
+    int mini=1e9;
+    for(int k=i;k<j;k++){
+        int steps= arr[i-1]*arr[k]*arr[j] + f(i,k,arr) + f(k+1,j,arr);
+        
+        if(steps<mini ){
+            mini=steps;
+        }
+    }
+    return mini;
+}
+    int matrixMultiplication(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        return f(1,n-1,arr);
+    }
+};
+
+memoization
+class Solution {
+public:
+int f(int i , int j , vector<int> &arr, vector<vector<int>> &dp){
+    if(i==j) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    int mini=1e9;
+    
+    for(int k=i;k<j;k++){
+        int steps= arr[i-1]*arr[k]*arr[j] + f(i,k,arr,dp) + f(k+1,j,arr,dp);
+        
+        if(steps<mini ){
+            mini=steps;
+        }
+    }
+    return dp[i][j]=mini;
+}
+    int matrixMultiplication(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        return f(1,n-1,arr,dp);
+    }
+};
