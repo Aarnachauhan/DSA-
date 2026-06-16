0c0d80e3648f14c19d1bf4b6b8366339f25446f8
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-16 12:47:28 +0400

    Update biparitite graph.cpp

--- Graph /biparitite graph.cpp
+++ Graph /biparitite graph.cpp
@@ -35,3 +35,51 @@ public:
         return true;
     }
 };
+
+dfs
+class Solution {
+public:
+
+    bool dfs(int node, int clr,
+             vector<vector<int>>& graph,
+             vector<int>& color)
+    {
+        color[node] = clr;
+
+        for(auto neigh : graph[node])
+        {
+            // uncolored node
+            if(color[neigh] == -1)
+            {
+                if(!dfs(neigh, !clr, graph, color))
+                    return false;
+            }
+
+            // already colored
+            else if(color[neigh] == color[node])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isBipartite(vector<vector<int>>& graph) {
+
+        int n = graph.size();
+
+        vector<int> color(n, -1);
+
+        for(int i = 0; i < n; i++)
+        {
+            if(color[i] == -1)
+            {
+                if(!dfs(i, 0, graph, color))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
