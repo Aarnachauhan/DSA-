1fba5d43850050f633f22f729b2c1cc4c78dd570
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-18 11:51:05 +0400

    Update set kth bit.cpp

--- bit manipulation/set kth bit.cpp
+++ bit manipulation/set kth bit.cpp
@@ -1,7 +1,12 @@
+gfg https://www.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1
+
 class Solution {
   public:
-    int setKthBit(int n, int k) {
-        // Code here
-        return n| (1<<k);
+    bool checkKthBit(int n, int k) {
+        //  code here
+        int ans=1<<k;
+        if((n&ans)!=0) return true;
+        return false;
+        
     }
 };
