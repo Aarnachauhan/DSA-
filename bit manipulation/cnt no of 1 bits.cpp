df45027c4e6123c0b9270c12b8c6753b343a01e8
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-18 12:31:07 +0400

    Update cnt no of 1 bits.cpp

--- bit manipulation/cnt no of 1 bits.cpp
+++ bit manipulation/cnt no of 1 bits.cpp
@@ -1,4 +1,18 @@
 gfg
+approach 1:
+  class Solution {
+  public:
+    int setBits(int n) {
+        // Write Your Code here
+        int ans=0;
+        while(n>0){
+            if((n&1)==1) ans++;
+            n=n>>1;
+        }
+        return ans;
+    }
+};
+approach 2:
 class Solution {
   public:
     int setBits(int n) {
