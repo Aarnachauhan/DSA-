3cb752f2b549aa7daac91bdf2d31c1f5cd8087c3
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-20 00:18:03 +0400

    Update divide two integers.cpp

--- bit manipulation/divide two integers.cpp
+++ bit manipulation/divide two integers.cpp
@@ -1,22 +1,32 @@
 leetcode 29
 tc- o(log2N)^2
+
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-      if(dividend == INT_MIN && divisor == -1){
+        if(dividend==INT_MIN && divisor==-1){
             return INT_MAX;
         }
-        long long dvd = labs(dividend), dvs = labs(divisor), result = 0;
-        int sign = (dividend > 0) ^ (divisor > 0) == 0 ? 1 : -1;
-        while(dvd >= dvs){ //log 2 N 
-            long long temp = dvs, mul = 1;
-            while(temp << 1 <= dvd){ //log 2 N
-                temp <<= 1;
-                mul <<= 1;
+        long long dvd=llabs((long long) dividend);
+        long long div=llabs((long long) divisor);
+
+        long long quo=0;
+        while(dvd>=div){
+            long long temp=div;
+            long long multi=1;
+
+            while((temp<<1)<dvd){
+                temp<<=1;
+                multi<<=1;
             }
-            dvd -= temp;
-            result += mul;
-        } 
-        return sign*result;
+            dvd-=temp;
+            quo+=multi;
+        }
+
+         bool negative =
+            (dividend < 0) ^ (divisor < 0);
+         if (negative)
+            quo = -quo;
+            return (int)quo;
     }
 };
