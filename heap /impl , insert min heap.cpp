41b55c942295598c367c823ecd6930c1675dd1f4
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-21 12:53:20 +0400

    Refactor min heap insertion into MinHeap class

--- heap /impl , insert min heap.cpp
+++ heap /impl , insert min heap.cpp
@@ -1,35 +1,50 @@
 no platform -> refer gfg
 
-in maxheap , we didnt put anything in index0 , but here ,we started from 0 . thats why parent= i-1/2 and not parent=i/2
 tc-o(logn )
 sc-o(n) 
 #include <bits/stdc++.h>
 using namespace std;
-
-void insert(vector<int> & heap , int value){
-    heap.push_back(value);
-    int index=heap.size()-1;
-    while(index > 0 && heap[(index-1)/2] > heap[index]){
-        swap(heap[index], heap[(index-1)/2]);
+#include <bits/stdc++.h>
+using namespace std;
+class MinHeap{
+    public:
+    vector<int> heap;
+    
+    void insert(int val){
+        heap.push_back(val);
+        int ind=heap.size()-1;
         
-        index=(index-1)/2;
+        while(ind>0){
+            int parent=(ind-1)/2;
+            
+            if(heap[parent]>heap[ind]){
+                swap(heap[parent],heap[ind]);
+                ind=parent;
+            }
+            else break;
+        }
     }
-
+    
+    void print(){
+        for(int x:heap){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
 };
 
+
+
 int main() {
-	// your code goes here
-	vector<int>arr;
-    vector<int>values = {10, 3, 2, 4, 5, 1};
-    int n = values.size();
-    for (int i = 0; i < n; i++) {
-        insert(arr, values[i]);
-        cout << "Inserted " << values[i]
-             << " into the min-heap: ";
-        for (int j = 0; j <arr.size(); j++) {
-            cout << arr[j] << " ";
-        }
-        cout << endl;
-    }
-    return 0;
+	MinHeap h;
+	h.insert(10);
+	h.insert(20);
+	h.insert(5);
+	h.insert(15);
+	h.insert(2);
+	
+	h.print();
+	
+	return 0;
+
 }
