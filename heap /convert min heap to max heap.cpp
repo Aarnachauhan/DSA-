bfee53bcc968bea987d5c12ed4897e8284a36dbf
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-21 17:16:43 +0400

    Create convert min heap to max heap.cpp

---
+++ heap /convert min heap to max heap.cpp
@@ -0,0 +1,28 @@
+tc-o(n)
+sc-o(logn)
+
+void maxheapify(vector<int> & arr , int n , int i){
+    int largest=i;
+	int left=2*i+1;
+	int right=2*i+2;
+
+	if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        maxheapify(arr, n, largest);
+    }
+}
+
+vector<int>MinToMaxHeap(int n, vector<int>&arr)
+{
+	for(int i=n/2 -1 ; i>=0 ; i--){
+		maxheapify(arr,n, i);
+	}
+	return arr;
+
+}
