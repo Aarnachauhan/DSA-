a461f3c42ee90795a35ac22c1d9889f9a4b2e790
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-21 21:43:56 +0400

    Update replace elements by rank.cpp

--- heap /replace elements by rank.cpp
+++ heap /replace elements by rank.cpp
@@ -25,3 +25,41 @@ public:
         return ans;
     }
 };
+
+using heap ( not a good solution)
+tc-o(nlogn)
+sc-o(n)
+vector<int> replaceWithRank(vector<int>& arr, int n)
+{
+    priority_queue<
+        int,
+        vector<int>,
+        greater<int>
+    > pq;
+
+    for(int x : arr)
+        pq.push(x);
+
+    unordered_map<int,int> rank;
+
+    int r = 1;
+
+    while(!pq.empty())
+    {
+        int x = pq.top();
+        pq.pop();
+
+        if(rank.find(x) == rank.end())
+        {
+            rank[x] = r;
+            r++;
+        }
+    }
+
+    vector<int> ans;
+
+    for(int x : arr)
+        ans.push_back(rank[x]);
+
+    return ans;
+}
