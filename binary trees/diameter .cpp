c72d08e3f3ef42624c8056efa99cf85d396da79b
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-24 20:46:09 +0400

    Update diameter .cpp

--- binary trees/diameter .cpp
+++ binary trees/diameter .cpp
@@ -3,20 +3,19 @@ tc-O(n)
 sc-o(n)
 
 //height is the no of nodes
-
 class Solution {
-  public:
-    int diameter(Node* root) {
-        // Your code here
+public:
+int height(TreeNode* node , int &dia){
+    if(node==nullptr) return 0;
+    int left=height(node->left,dia);
+    int right=height(node->right,dia);
+     dia=max(dia,left+right);
+     return 1+ max(left,right);
+    
+}
+    int diameterOfBinaryTree(TreeNode* root) {
         int dia=0;
         height(root,dia);
         return dia;
     }
-    private:
-    int height(Node* root, int &dia){
-        if(root==NULL) return 0;
-        int l=height(root->left,dia);
-        int r=height(root->right,dia);
-        dia=max(dia,l+r);
-        return 1+max(l,r);
-    }
+};
