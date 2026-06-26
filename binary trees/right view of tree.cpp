623922682b5b1508e20896fc83ca4bd1e9e5b1a0
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-26 15:32:25 +0400

    Update right view of tree.cpp

--- binary trees/right view of tree.cpp
+++ binary trees/right view of tree.cpp
@@ -1,4 +1,48 @@
 lc 199
+
+better:
+tc-o(n)
+    sc-o(n)
+class Solution {
+public:
+
+    vector<int> rightSideView(TreeNode* root) {
+
+        vector<int> ans;
+
+        if(root==nullptr)
+            return ans;
+
+        queue<TreeNode*> q;
+
+        q.push(root);
+
+        while(!q.empty()){
+
+            int size=q.size();
+
+            for(int i=0;i<size;i++){
+
+                TreeNode* node=q.front();
+                q.pop();
+
+                if(i==size-1)
+                    ans.push_back(node->val);
+
+                if(node->left)
+                    q.push(node->left);
+
+                if(node->right)
+                    q.push(node->right);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+optimal:
 tc-o(n)
 sc-o(H) where h is the height of the tree.
 
