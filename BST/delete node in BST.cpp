e7dc7977bfa02690c6a155a81742877e5901ff16
Aarna chauhan <chauhanaarna6@gmail.com>
2026-06-28 01:18:57 +0400

    Update delete node in BST.cpp

--- BST/delete node in BST.cpp
+++ BST/delete node in BST.cpp
@@ -1,16 +1,57 @@
 lc 450
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+recursive:
+class Solution {
+public:
+
+    TreeNode* findMin(TreeNode* root){
+        while(root->left)
+            root = root->left;
+
+        return root;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+
+        if(root == nullptr)
+            return nullptr;
+
+        if(key < root->val){
+            root->left = deleteNode(root->left, key);
+        }
+
+        else if(key > root->val){
+            root->right = deleteNode(root->right, key);
+        }
+
+        else{
+
+            // Case 1 & 2
+            if(root->left == nullptr){
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+
+            if(root->right == nullptr){
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            // Case 3
+            TreeNode* successor = findMin(root->right);
+
+            root->val = successor->val;
+
+            root->right = deleteNode(root->right, successor->val);
+        }
+
+        return root;
+    }
+};
+
+iterative:
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
