687fdbcf343c270c21b5304be1bc6778ee3971a3
Aarna chauhan <chauhanaarna6@gmail.com>
2026-07-01 20:42:33 +0400

    Update leaders.cpp

--- Array/leaders.cpp
+++ Array/leaders.cpp
@@ -16,3 +16,24 @@ vector<int> superiorElements(vector<int>&a) {
     return ans;
 
 }
+
+
+gfg
+class Solution {
+  public:
+    vector<int> leaders(vector<int>& arr) {
+        // code here
+        int maxi=0;
+        int n=arr.size();
+        vector<int> ans;
+        for(int i=n-1;i>=0;i--){
+            if(arr[i]>=maxi){
+                ans.push_back(arr[i]);
+                
+            }
+            maxi=max(maxi,arr[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
