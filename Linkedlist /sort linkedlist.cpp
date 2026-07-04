f67105d4423884607a687fffe3a2008cf373f677
Aarna chauhan <chauhanaarna6@gmail.com>
2026-07-04 21:40:12 +0400

    Update sort linkedlist.cpp

--- Linkedlist /sort linkedlist.cpp
+++ Linkedlist /sort linkedlist.cpp
@@ -24,79 +24,67 @@ public:
 };
 
 optimal:
+tc-o(nlogn)
+sc-O(nlogn)
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-
-ListNode* merge(ListNode* list1, ListNode* list2) {
-        // Create a dummy node
-        ListNode* dummyNode = new ListNode(-1);
-        
-        // Temp pointer to build merged list
-        ListNode* temp = dummyNode;
-
-        // Traverse both lists
-        while (list1 != nullptr && list2 != nullptr) {
-            // Choose smaller node
-            if (list1->val <= list2->val) {
-                temp->next = list1;
-                list1 = list1->next;
-            } else {
-                temp->next = list2;
-                list2 = list2->next;
-            }
-            // Move temp pointer
-            temp = temp->next;
-        }
-
-        // Attach remaining nodes
-        if (list1 != nullptr) {
-            temp->next = list1;
-        } else {
-            temp->next = list2;
-        }
-
-        // Return head of merged list
-        return dummyNode->next;
+ListNode* merge(ListNode* left , ListNode* right){
+    if(left==nullptr) return right;
+    if(right==nullptr) return left;
+    ListNode *dummy=new ListNode(0);
+    ListNode *temp=dummy;
+    while(left!=nullptr && right!=nullptr){
+     if(left->val < right->val){
+        temp->next=left;
+        temp=left;
+        left=left->next;
+     }
+     else{
+        temp->next=right;
+        temp=right;
+        right=right->next;
+     }
     }
-ListNode* findmid(ListNode* head){
-    if(head==nullptr || head->next==nullptr){
-        return  head;
+
+    while(left!=nullptr){
+        temp->next=left;
+        temp=left;
+        left=left->next;
     }
-     ListNode* slow = head;
-        ListNode* fast = head->next;
 
-
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        return slow;
+    while(right!=nullptr){
+        temp->next=right;
+        temp=right;
+        right=right->next;
+    }
+    dummy=dummy->next;
+    return dummy;
 }
+ListNode* getmiddle(ListNode* head){
+    ListNode* slow=head;
+    ListNode* fast=head->next;
 
-
+    while(fast && fast->next!=nullptr){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
     ListNode* sortList(ListNode* head) {
-        if(head==nullptr || head->next==nullptr) return head;
-        ListNode* middle=findmid(head);
+        if(head==nullptr || head->next ==nullptr) return head;
 
-        ListNode* right=middle->next;
-        middle->next=nullptr;
+        ListNode* mid=getmiddle(head);
+
         ListNode* left=head;
+        ListNode* right=mid->next;
+        mid->next=nullptr;
 
         left=sortList(left);
         right=sortList(right);
 
-        return merge(left,right);
+        ListNode* result=merge(left,right);
+
+        return result;
     }
 };
