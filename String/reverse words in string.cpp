ddb69f13930ed9f06009e9af8622efc6c41010aa
Aarna chauhan <chauhanaarna6@gmail.com>
2026-07-06 12:54:49 +0400

    Update reverse words in string.cpp

--- String/reverse words in string.cpp
+++ String/reverse words in string.cpp
@@ -1,6 +1,36 @@
 lc 151
 tc-O(n) , as both for and while loop is updating the same i
 sc-o(n)
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.length();
+        string ans="";
+
+        reverse(s.begin(),s.end());
+
+        for(int i=0;i<n;i++){
+            string word="";
+            while(i<n && s[i]!=' '){
+                word+=s[i];
+                i++;
+            }
+             reverse(word.begin(),word.end());
+        if(word.length()>0){
+            ans+=" "+ word;
+        }
+
+        }
+         return ans.substr(1);
+       
+    }
+};
+
+
+tc-o(n)
+sc-o(n)
 class Solution {
 public:
     string reverseWords(string s) {
