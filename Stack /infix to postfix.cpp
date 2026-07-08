d9e43b38da9c3e5e35cdfec9b2238dd68d92a6d0
Aarna chauhan <chauhanaarna6@gmail.com>
2026-07-08 14:01:05 +0400

    Update infix to postfix.cpp

--- Stack /infix to postfix.cpp
+++ Stack /infix to postfix.cpp
@@ -2,50 +2,53 @@ gfg
 tc-O(n) +o(n)
 sc-o(n)+O(n)
   result + stack
-
-public:
-    // Function to convert an infix expression to a postfix expression.
-    int prec(char c){
-        if(c=='^') return 3;
-        else if(c=='/' || c=='*') return 2;
-        else if(c=='+' || c=='-') return 1;
-        else return -1;
-    }
-    
-
+//this code handles ^ this case also
+class Solution {
+  public:
+  
+ int prec(char str){
+     if(str=='^') return 3;
+     else if(str=='*' || str=='/') return 2;
+     else if(str=='+' || str=='-') return 1;
+     else return -1;
+ }
     string infixToPostfix(string& s) {
-        // Your code here
+        // code here
         stack<char> st;
         string result;
         int n=s.length();
         for(int i=0;i<n;i++){
             char c=s[i];
-            if((c>='a' && c<='z') ||(c>='A' && c<='Z') ||(c>='0' && c<='9') ){
+            if((c>='a' && c<='z') || (c>='A' && c<='Z') || (c>='0' && c<='9')){
                 result+=c;
             }
             else if(c=='('){
                 st.push('(');
             }
             else if(c==')'){
-                while(st.top()!='('){ //o(n)
+                while(st.top()!='('){
                     result+=st.top();
                     st.pop();
                 }
-                st.pop(); // ( isko bhi pop krdo
+                st.pop();
             }
             else{
-                while(!st.empty() && prec(s[i])<=prec(st.top())){ //o(n)
-                    result+=st.top();
-                    st.pop();
-                }
-                st.push(c);
+                while(!st.empty() &&
+                    st.top() != '(' &&
+                        (prec(st.top()) > prec(c) ||
+                          (prec(st.top()) == prec(c) && c != '^')))
+                                {
+                                    result += st.top();
+                                    st.pop();
+            }
+            st.push(c);
             }
         }
         while(!st.empty()){
             result+=st.top();
             st.pop();
         }
-       return result; 
+        return result;
     }
 };
-  
+
