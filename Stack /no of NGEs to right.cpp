f2695550dcd07d6e523cb2e4b9270cdf4eee7ce9
Aarna chauhan <chauhanaarna6@gmail.com>
2026-07-09 18:53:43 +0400

    Update no of NGEs to right.cpp

--- Stack /no of NGEs to right.cpp
+++ Stack /no of NGEs to right.cpp
@@ -1,7 +1,34 @@
 gfg
 we have a optimal approach also - binary tree
+brute force
+tc-o(n2)
+sc-o(n)
+
+class Solution {
+  public:
+    vector<int> countGreater(vector<int> &arr, vector<int> &indices) {
+        // code here
+        int n=arr.size();
+        vector<int> ans;
+       for(int i=0;i<indices.size();i++){
+           int ind=indices[i];
+           int cnt=0;
+            for(int j=ind+1;j<n;j++){
+                if(arr[j]>arr[ind]){
+                    cnt++;
+                }
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
+
+
 
 better approach:
+tc-o(nlogn)
+sc-O(n)
 class Solution {
   public:
 
