71187e6ae476f06b9fb91300ff791d7f8b4ecc2a
Aarna chauhan <chauhanaarna6@gmail.com>
2026-07-14 12:51:47 +0400

    Update impl , insert max heap.cpp

--- heap /impl , insert max heap.cpp
+++ heap /impl , insert max heap.cpp
@@ -1,6 +1,51 @@
 no platform-- codechef
 output : 55 54 53 50 52 
 tc-o(logn )
+
+my code:
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+class Maxheap{
+    public:
+    vector<int> heap;
+    
+    void insert(int val){
+        heap.push_back(val);
+        int ind=heap.size()-1;
+        while(ind>0){
+            int parent=(ind-1)/2;
+            if(heap[parent]<heap[ind]){
+                swap(heap[parent],heap[ind]);
+                parent=ind;
+            }else break;
+        }
+        
+    }
+    
+    void print(){
+        for(int x:heap){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+
+
+int main(){
+    Maxheap h;
+    h.insert(50);
+	h.insert(55);
+	h.insert(53);
+	h.insert(52);
+	h.insert(54);
+	h.print();
+
+}
+
+older code:
 #include <bits/stdc++.h>
 using namespace std;
 
